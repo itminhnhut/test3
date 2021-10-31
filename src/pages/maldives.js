70abcabb09cd99b110c505352d198d10c282ef2a
Khoa D. Duy <khoadd@nami.trade>
2021-10-31 18:34:08 +0700

    Prevent Maldives landing page

--- src/pages/maldives.js
+++ src/pages/maldives.js
@@ -1,7 +1,11 @@
 import LandingPage from 'version/maldives/v0.1/LandingPage'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import { ___DEV___ } from 'utils'
 
-const Maldives = () => <LandingPage/>
+const Maldives = () => {
+    if (!___DEV___) return null
+    return <LandingPage/>
+}
 
 export const getStaticProps = async ({ locale }) => ({
     props: {
