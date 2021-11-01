c371a3c4359bba9c91bad7150a98db9c9fa3a248
Khoa D. Duy <khoadd@nami.trade>
2021-11-01 10:21:36 +0700

    Fix sparkline

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,4 +1,5 @@
 import DOMPurify from 'dompurify';
+import { log } from 'utils/index'
 
 export const truncate = (fullStr, strLen, separator) => {
     if (fullStr.length <= strLen) return fullStr;
@@ -24,9 +25,12 @@ export const sanitize = (content) => {
 export const ___DEV___ = process.env.NODE_ENV === 'development'
 
 export const sparkLineBuilder = (symbol, color) => {
-    if (___DEV___) {
-        return `https://data-test.bitbattle.io/api/v1/chart/sparkline?symbol=${symbol}&broker=NAMI_SPOT&color=%23${color.replace('#', '')}`
-    } else {
-        return `https://data.bitbattle.io/api/v1/chart/sparkline?symbol=${symbol}&broker=NAMI_SPOT&color=%23${color.replace('#', '')}`
-    }
+    // log.d('check env____', process.env.NEXT_PUBLIC_PRICE_API_URL)
+    // if (___DEV___) {
+    //     return `https://data-test.bitbattle.io/api/v1/chart/sparkline?symbol=${symbol}&broker=NAMI_SPOT&color=%23${color.replace('#', '')}`
+    // } else {
+    //     return `https://datav2.nami.exchange/api/v1/chart/sparkline?symbol=${symbol}&broker=NAMI_SPOT&color=%23${color.replace('#', '')}`
+    // }
+
+    return `${process.env.NEXT_PUBLIC_PRICE_API_URL}/api/v1/chart/sparkline?symbol=${symbol}&broker=NAMI_SPOT&color=%23${color.replace('#', '')}`
 }
