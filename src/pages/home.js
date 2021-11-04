a05f22c0cccb91979fd98aebb8713f8ad0e4962b
trungnd <trungnd@attlas.io>
2021-11-04 23:46:44 +0700

    Add home page

---
+++ src/pages/home.js
@@ -0,0 +1,12 @@
+import HomePage from 'version/maldives/m1/HomePage/HomePage'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+
+const Index = () => <HomePage/>
+
+export const getStaticProps = async ({ locale }) => ({
+    props: {
+        ...await serverSideTranslations(locale, ['common', 'navbar', 'home', 'modal', 'input', 'table']),
+    },
+});
+
+export default Index
