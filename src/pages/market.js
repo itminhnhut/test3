d3b77ed165f44f18dc93c54227ecf62e17b613de
Khoa D. Duy <khoadd@nami.trade>
2021-11-05 18:13:10 +0700

    Update

--- src/pages/market.js
+++ src/pages/market.js
@@ -1,13 +1,13 @@
-import MarketIndex from 'version/maldives/m1/Market'
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
-
-const Market = () => <MarketIndex/>
-
-export const getStaticProps = async ({ locale }) => ({
-    props: {
-        ...await serverSideTranslations(locale, ['common', 'navbar']),
-    },
-});
-
-
-export default Market
+// import MarketIndex from 'version/maldives/m1/Market'
+// import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+//
+// const Market = () => <MarketIndex/>
+//
+// export const getStaticProps = async ({ locale }) => ({
+//     props: {
+//         ...await serverSideTranslations(locale, ['common', 'navbar']),
+//     },
+// });
+//
+//
+// export default Market
