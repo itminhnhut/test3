967b737e6b4e7c161301948b69dca8f6e6450ade
trungnd <trungnd@nami.trade>
2021-11-10 16:27:53 +0700

    Fix head

--- src/components/common/Head.js
+++ src/components/common/Head.js
@@ -15,25 +15,29 @@ const Head = () => {
                 <meta name="description" content="Nami - Nơi giao dịch tiền điện tử cho người dùng cần sự đơn giản, nhanh chóng, đòn bẩy cao (lên tới x100), phí thấp, ổn định." />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <link rel="manifest" href="/site.webmanifest" key="site-manifest" />
-                {
-                    route !== '/blog/[slug]' && (
-                        <>
-                            <meta name="twitter:card" content="summary" />
-                            <meta name="twitter:creator" content="@Nami" />
-                            <meta name="twitter:site" content="@Nami" />
-                            <meta property="og:title" content="Nami Exchange" />
-                            <meta property="og:description" content="Sàn giao dịch Spot và Futures an toàn" />
-                            <meta property="og:url" content="https://nami.exchange" key="og-url" />
-                            <meta name="twitter:title" content="Nami Exchange" />
-                            <meta name="twitter:description" content="Sàn giao dịch Spot và Futures an toàn" />
-                            <meta name="twitter:url" content="https://nami.exchange" />
-                            <meta name="description" content="Sàn giao dịch Spot và Futures an toàn" />
-                        </>
-                    )
-                }
-
+                <>
+                    <meta name="twitter:card" content="summary" />
+                    <meta name="twitter:creator" content="@Nami" />
+                    <meta name="twitter:site" content="@Nami" />
+                    <meta property="og:title" content="Nami Exchange" />
+                    <meta property="og:description" content="Sàn giao dịch Spot và Futures an toàn" />
+                    <meta property="og:url" content="https://nami.exchange" key="og-url" />
+                    <meta name="twitter:title" content="Nami Exchange" />
+                    <meta name="twitter:description" content="Sàn giao dịch Spot và Futures an toàn" />
+                    <meta name="twitter:url" content="https://nami.exchange" />
+                    <meta name="description" content="Sàn giao dịch Spot và Futures an toàn" />
+                    <meta
+                        property="og:image"
+                        content="https://nami.exchange/images_v2/featured/common-featured.png"
+                        key="fb-image"
+                    />
+                    <meta
+                        name="twitter:image"
+                        content="https://nami.exchange/images_v2/featured/common-featured.png"
+                        key="twitter-image"
+                    />
+                </>
                 <link rel="icon" type="image/x-icon" href="/favicon.ico" />
-
                 {/* fix fontawesome always huge */}
                 <style>{dom.css()}</style>
             </NextHead>
