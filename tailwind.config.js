2f899b26a4e132ddc9d308e4b1f97406cf6d0d4e
Khoa D. Duy <khoadd@nami.trade>
2021-12-02 16:59:40 +0700

    Fix minor

--- tailwind.config.js
+++ tailwind.config.js
@@ -31,7 +31,7 @@ module.exports = {
     mode: 'jit',
     purge: [
         './src/pages/**/*.{js,ts,jsx,tsx}',
-        './src/components/**/*.{js,ts,jsx,tsx}',
+        './src/components/**/**/*.{js,ts,jsx,tsx}',
         './src/version/**/*.{js,ts,jsx,tsx}'
     ],
     darkMode: 'class',
