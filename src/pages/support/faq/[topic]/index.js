a54ff64a7e93156851602a7d2bf328dbec3a5db1
Khoa D. Duy <khoadd@nami.trade>
2022-03-15 11:02:01 +0700

    Fixed change artical route

--- src/pages/support/faq/[topic]/index.js
+++ src/pages/support/faq/[topic]/index.js
@@ -6,7 +6,7 @@ import { ChevronLeft } from 'react-feather'
 import TopicsLayout from 'components/screens/Support/TopicsLayout'
 import Link from 'next/link'
 import useApp from 'hooks/useApp'
-import { appUrlHandler, SupportCategories } from 'constants/faqHelper'
+import { SupportCategories } from 'constants/faqHelper'
 import { useTranslation } from 'next-i18next'
 import { useCallback, useEffect, useState } from 'react'
 import { ghost } from 'utils'
@@ -19,32 +19,55 @@ const FaqTopics = (props) => {
 
     const router = useRouter()
     const isApp = useApp()
-    const { t, i18n: { language } } = useTranslation()
-    const cats = SupportCategories.faq[language]?.find(o => o.displaySlug === router?.query?.topic)?.subCats || false
+    const {
+        t,
+        i18n: { language },
+    } = useTranslation()
+    const cats =
+        SupportCategories.faq[language]?.find(
+            (o) => o.displaySlug === router?.query?.topic
+        )?.subCats || false
 
     const renderGroup = () => {
-        return cats && cats.map(item => (
-            <div key={item.id} title={item.title} className="mb-[18px] h-full w-full sm:w-1/2 lg:w-1/3 sm:pr-3">
-                <Link href={{
-                    pathname: PATHS.SUPPORT.FAQ + `/${router?.query?.topic}`,
-                    query: appUrlHandler({group: item.displaySlug}, isApp)
-                }} key={item.uuid}>
-                    <a className="truncate block bg-gray-4 dark:bg-darkBlue-4 rounded-sm hover:opacity-80 px-4 py-3 text-sm font-medium lg:text-[16px]">
-                        {item?.title}
-                    </a>
-                </Link>
-            </div>
-        ))
+        return (
+            cats &&
+            cats.map((item) => (
+                <div
+                    key={item.id}
+                    title={item.title}
+                    className='mb-[18px] h-full w-full sm:w-1/2 lg:w-1/3 sm:pr-3'
+                >
+                    <Link
+                        href={{
+                            pathname:
+                                PATHS.SUPPORT.FAQ + `/${router?.query?.topic}`,
+                            query: appUrlHandler(
+                                { group: item.displaySlug },
+                                isApp
+                            ),
+                        }}
+                        key={item.uuid}
+                    >
+                        <a className='truncate block bg-gray-4 dark:bg-darkBlue-4 rounded-sm hover:opacity-80 px-4 py-3 text-sm font-medium lg:text-[16px]'>
+                            {item?.title}
+                        </a>
+                    </Link>
+                </div>
+            ))
+        )
     }
 
     const renderAppHeader = () => {
         if (!isApp) return null
-        const topic = props?.data?.tags?.find(o => o?.displaySlug === router?.query?.topic)?.name
+        const topic = props?.data?.tags?.find(
+            (o) => o?.displaySlug === router?.query?.topic
+        )?.name
         return (
-            <div onClick={router?.back}
-                 className="active:text-dominant flex items-center px-4 pt-4 pb-2 text-sm font-medium">
-                <ChevronLeft size={16}
-                             className="mr-2.5"/>
+            <div
+                onClick={router?.back}
+                className='active:text-dominant flex items-center px-4 pt-4 pb-2 text-sm font-medium'
+            >
+                <ChevronLeft size={16} className='mr-2.5' />
                 {topic}
                 {topic && ' | '}
                 Nami FAQ
@@ -55,8 +78,10 @@ const FaqTopics = (props) => {
     const renderGroupArticles = useCallback(() => {
         if (!currentGroup || !articles || !articles.length) return null
 
-        const data = articles?.filter(e => {
-            const isBelongThisGroup = e?.tags?.find(o => o.slug === `faq-${language}-${currentGroup}`)
+        const data = articles?.filter((e) => {
+            const isBelongThisGroup = e?.tags?.find(
+                (o) => o.slug === `faq-${language}-${currentGroup}`
+            )
             return !!isBelongThisGroup
         })
 
@@ -66,18 +91,22 @@ const FaqTopics = (props) => {
             return <div>{t('support-center:no_articles')}</div>
         }
 
-        return data?.map(article => (
-            <Link href={{
-                pathname: PATHS.SUPPORT.FAQ + `/${router?.query?.topic}/[articles]`,
-                query: appUrlHandler({ articles: article.slug.toString() }, isApp)
-            }} key={article.uuid}>
-                <a className="block text-sm font-medium mb-[18px] lg:text-[16px] lg:mb-8 hover:!text-dominant">
-                    {article?.title}{' '}{' '}
-                    <span className="text-[10px] lg:text-xs text-txtSecondary dark:text-txtSecondary-dark">
-                        {formatTime(article.created_at, 'dd-MM-yyyy')}
-                    </span>
-                </a>
-            </Link>
+        return data?.map((article) => (
+            <a
+                href={
+                    PATHS.SUPPORT.FAQ +
+                    `/${router?.query?.topic}/${article.slug}${
+                        isApp ? '?source=app' : ''
+                    }`
+                }
+                key={article.uuid}
+                className='block text-sm font-medium mb-[18px] lg:text-[16px] lg:mb-8 hover:!text-dominant'
+            >
+                {article?.title}{' '}
+                <span className='text-[10px] lg:text-xs text-txtSecondary dark:text-txtSecondary-dark'>
+                    {formatTime(article.created_at, 'dd-MM-yyyy')}
+                </span>
+            </a>
         ))
     }, [currentGroup, articles, language])
 
@@ -90,14 +119,22 @@ const FaqTopics = (props) => {
 
         const data = articles.slice(0, !!cats?.length ? 5 : 25)
 
-        return data.map(article => (
-            <Link href={{
-                pathname: PATHS.SUPPORT.FAQ + `/${router?.query?.topic}/[articles]`,
-                query: appUrlHandler({ articles: article.slug.toString() }, isApp)
-            }} key={article.uuid}>
-                <a className="!block !w-full text-sm font-medium mb-[18px] lg:text-[16px] lg:mb-8 hover:!text-dominant">
-                    {article?.title}{' '}{' '}
-                    <span className="text-[10px] lg:text-xs text-txtSecondary dark:text-txtSecondary-dark">
+        return data.map((article) => (
+            <Link
+                href={{
+                    pathname:
+                        PATHS.SUPPORT.FAQ +
+                        `/${router?.query?.topic}/[articles]`,
+                    query: appUrlHandler(
+                        { articles: article.slug.toString() },
+                        isApp
+                    ),
+                }}
+                key={article.uuid}
+            >
+                <a className='!block !w-full text-sm font-medium mb-[18px] lg:text-[16px] lg:mb-8 hover:!text-dominant'>
+                    {article?.title}{' '}
+                    <span className='text-[10px] lg:text-xs text-txtSecondary dark:text-txtSecondary-dark'>
                         {formatTime(article.created_at, 'dd-MM-yyyy')}
                     </span>
                 </a>
@@ -116,43 +153,47 @@ const FaqTopics = (props) => {
     return (
         <>
             {renderAppHeader()}
-            <TopicsLayout useTopicTitle mode="faq" faqCurrentGroup={currentGroup}>
-                <div className={
-                    classNames('flex flex-wrap',
-                               {
-                                   'mb-4 md:mb-6': !!cats.length && !!!currentGroup
-                               })}>
+            <TopicsLayout
+                useTopicTitle
+                mode='faq'
+                faqCurrentGroup={currentGroup}
+            >
+                <div
+                    className={classNames('flex flex-wrap', {
+                        'mb-4 md:mb-6': !!cats.length && !!!currentGroup,
+                    })}
+                >
                     {!currentGroup && renderGroup()}
                 </div>
-                <div className="">
-                    {renderGroupArticles()}
-                </div>
-                {!!cats.length && !!articles?.length && !!!currentGroup
-                && <div className="text-[16px] font-bold md:text-[20px] lg:text-[28px] mb-6">
-                    {t('support-center:lasted_articles')}
-                </div>}
-                <div className="">
-                    {renderLastedArticles()}
-                </div>
+                <div className=''>{renderGroupArticles()}</div>
+                {!!cats.length && !!articles?.length && !!!currentGroup && (
+                    <div className='text-[16px] font-bold md:text-[20px] lg:text-[28px] mb-6'>
+                        {t('support-center:lasted_articles')}
+                    </div>
+                )}
+                <div className=''>{renderLastedArticles()}</div>
             </TopicsLayout>
         </>
     )
 }
 
 export async function getServerSideProps({ locale, query }) {
-    const articles = await ghost.posts.browse(
-        {
-            filter: `tags:faq-${locale || 'en'}-${query?.topic}`,
-            include: `tags`
-        })
+    const articles = await ghost.posts.browse({
+        filter: `tags:faq-${locale || 'en'}-${query?.topic}`,
+        include: `tags`,
+    })
 
     return {
         props: {
             data: {
-                articles
+                articles,
             },
-            ...await serverSideTranslations(locale, ['common', 'navbar', 'support-center'])
-        }
+            ...(await serverSideTranslations(locale, [
+                'common',
+                'navbar',
+                'support-center',
+            ])),
+        },
     }
 }
 
