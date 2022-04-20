e0d8a96be318d68738d7d5cfb58648a634763627
Phú Dương <phuduong@Phus-MacBook-Pro.local>
2022-04-20 11:51:40 +0700

    Add share url

--- src/components/common/Head.js
+++ src/components/common/Head.js
@@ -5,6 +5,8 @@ import seoConfigs from '../../config/seo.json'
 
 configFont.autoAddCss = false;
 
+const APP_URL = process.env.APP_URL
+
 const Head = ({language = 'vi'}) => {
     const router = useRouter();
     const {route} = router;
@@ -24,12 +26,12 @@ const Head = ({language = 'vi'}) => {
                     <meta name="twitter:card" content="summary"/>
                     <meta name="twitter:creator" content="@Nami"/>
                     <meta name="twitter:site" content="@Nami"/>
-                    <meta property="og:title" content="Nami Exchange"/>
-                    <meta property="og:description" content="Sàn giao dịch Spot và Futures an toàn"/>
-                    <meta property="og:url" content="https://nami.exchange" key="og-url"/>
-                    <meta name="twitter:title" content="Nami Exchange"/>
-                    <meta name="twitter:description" content="Sàn giao dịch Spot và Futures an toàn"/>
-                    <meta name="twitter:url" content="https://nami.exchange"/>
+                    <meta property="og:title" content={seoConfig.title[language]}/>
+                    <meta property="og:description" content={seoConfig.description[language]}/>
+                    <meta property="og:url" content={APP_URL + route} key="og-url"/>
+                    <meta name="twitter:title" content={seoConfig.title[language]}/>
+                    <meta name="twitter:description" content={seoConfig.description[language]}/>
+                    <meta name="twitter:url" content={APP_URL + route}/>
                     <meta
                         property="og:image"
                         content="https://static.namifutures.com/nami.exchange/images/common-featured.png"
