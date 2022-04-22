70dd72880ebee0d4751079e2df2954dc9d3936ef
trungnd <trungnd@attlas.io>
2022-04-22 23:15:35 +0700

    Remove close by condition

--- src/components/screens/Futures/PlaceOrder/Vndc/CloseAllOrders.js
+++ src/components/screens/Futures/PlaceOrder/Vndc/CloseAllOrders.js
@@ -102,37 +102,37 @@ const CloseAllOrders = () => {
                     </div>
                 </div>
             </Modal>
-            <Popover className="relative float-right h-[max-content]">
-                {({ open, close }) => (
-                    <>
-                        <Popover.Button >
-                            <div className="px-[8px] capitalize flex items-center py-[1px] mr-2 text-xs font-medium cursor-pointer hover:opacity-80 rounded-md">
-                                {t('futures:close_all:close_all_title')}
-                                <ChevronDown size={16} className="ml-1" />
-                            </div>
-                        </Popover.Button>
-                        <Transition
-                            as={Fragment}
-                            enter="transition ease-out duration-200"
-                            enterFrom="opacity-0 translate-y-1"
-                            enterTo="opacity-100 translate-y-0"
-                            leave="transition ease-in duration-150"
-                            leaveFrom="opacity-100 translate-y-0"
-                            leaveTo="opacity-0 translate-y-1"
-                        >
-                            <Popover.Panel className="absolute left-0 z-50 bg-white">
-                                <div className="min-w-[134px] pl-[12px] py-[8px] shadow-onlyLight font-medium text-xs flex flex-col">
-                                    <span onClick={() => onDeleteMode('ALL')} className={`text-darkBlue py-[1px] my-[2px] hover:text-teal cursor-pointer`}>{t('futures:close_all:close_all')}</span>
-                                    <span onClick={() => onDeleteMode('PROFIT')} className={`text-darkBlue py-[1px] my-[2px] hover:text-teal cursor-pointer`}>{t('futures:close_all:close_profit')}</span>
-                                    <span onClick={() => onDeleteMode('LOSS')} className={`text-darkBlue py-[1px] my-[2px] hover:text-teal cursor-pointer`}>{t('futures:close_all:close_loss')}</span>
-                                    <span onClick={() => onDeleteMode('PENDING')} className={`text-darkBlue py-[1px] my-[2px] hover:text-teal cursor-pointer`}>{t('futures:close_all:close_pending')}</span>
-                                    <span onClick={() => onDeleteMode('ACTIVE')} className={`text-darkBlue py-[1px] my-[2px] hover:text-teal cursor-pointer`}>{t('futures:close_all:close_active')}</span>
-                                </div>
-                            </Popover.Panel>
-                        </Transition>
-                    </>
-                )}
-            </Popover>
+            {/*<Popover className="relative float-right h-[max-content]">*/}
+            {/*    {({ open, close }) => (*/}
+            {/*        <>*/}
+            {/*            <Popover.Button >*/}
+            {/*                <div className="px-[8px] capitalize flex items-center py-[1px] mr-2 text-xs font-medium cursor-pointer hover:opacity-80 rounded-md">*/}
+            {/*                    {t('futures:close_all:close_all_title')}*/}
+            {/*                    <ChevronDown size={16} className="ml-1" />*/}
+            {/*                </div>*/}
+            {/*            </Popover.Button>*/}
+            {/*            <Transition*/}
+            {/*                as={Fragment}*/}
+            {/*                enter="transition ease-out duration-200"*/}
+            {/*                enterFrom="opacity-0 translate-y-1"*/}
+            {/*                enterTo="opacity-100 translate-y-0"*/}
+            {/*                leave="transition ease-in duration-150"*/}
+            {/*                leaveFrom="opacity-100 translate-y-0"*/}
+            {/*                leaveTo="opacity-0 translate-y-1"*/}
+            {/*            >*/}
+            {/*                <Popover.Panel className="absolute left-0 z-50 bg-white">*/}
+            {/*                    <div className="min-w-[134px] pl-[12px] py-[8px] shadow-onlyLight font-medium text-xs flex flex-col">*/}
+            {/*                        <span onClick={() => onDeleteMode('ALL')} className={`text-darkBlue py-[1px] my-[2px] hover:text-teal cursor-pointer`}>{t('futures:close_all:close_all')}</span>*/}
+            {/*                        <span onClick={() => onDeleteMode('PROFIT')} className={`text-darkBlue py-[1px] my-[2px] hover:text-teal cursor-pointer`}>{t('futures:close_all:close_profit')}</span>*/}
+            {/*                        <span onClick={() => onDeleteMode('LOSS')} className={`text-darkBlue py-[1px] my-[2px] hover:text-teal cursor-pointer`}>{t('futures:close_all:close_loss')}</span>*/}
+            {/*                        <span onClick={() => onDeleteMode('PENDING')} className={`text-darkBlue py-[1px] my-[2px] hover:text-teal cursor-pointer`}>{t('futures:close_all:close_pending')}</span>*/}
+            {/*                        <span onClick={() => onDeleteMode('ACTIVE')} className={`text-darkBlue py-[1px] my-[2px] hover:text-teal cursor-pointer`}>{t('futures:close_all:close_active')}</span>*/}
+            {/*                    </div>*/}
+            {/*                </Popover.Panel>*/}
+            {/*            </Transition>*/}
+            {/*        </>*/}
+            {/*    )}*/}
+            {/*</Popover>*/}
         </>
     );
 };
