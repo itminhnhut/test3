6c005db36d6fce0319530876d50ae04c9b3529b2
nhatlong1303 <53816049+nhatlong1303@users.noreply.github.com>
2022-05-23 13:43:06 +0700

    VNDC mobile - language

--- src/components/screens/Mobile/Futures/PlaceOrder/OrderButtonMobile.js
+++ src/components/screens/Mobile/Futures/PlaceOrder/OrderButtonMobile.js
@@ -20,7 +20,7 @@ const OrderButtonMobile = ({
     const isBuy = VndcFutureOrderType.Side.BUY === side
     const _price = getPrice(getType(type), side, price, pairPrice?.ask, pairPrice?.bid, stopPrice);
     
-    
+
     const onHandleSave = () => {
         if (!isAuth) {
             window.open(
@@ -51,7 +51,7 @@ const OrderButtonMobile = ({
 
     return (
         <div onClick={onHandleSave} className={`${isBuy ? 'bg-dominant' : 'bg-red'} text-white text-sm h-[67px] rounded-[6px] flex flex-col items-center justify-center ${classNameError}`}>
-            <div className='font-semibold text-center'>{!isAuth ? t('futures:mobile:login_short') : isBuy ? t('common:buy') + '/Long' : t('common:sell') + '/Short'}</div>
+            <div className='font-semibold text-center'>{!isAuth ? t('futures:mobile:login_short') : isBuy ? t('common:buy') : t('common:sell')}</div>
             <div className='font-medium'>{formatNumber(_price, decimals.decimalScalePrice, 0, true)}</div>
         </div>
     );
