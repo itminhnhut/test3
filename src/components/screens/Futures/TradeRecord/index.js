c5169dca1eead07136b6985f4f600d8a5ce60f59
nhatlong1303 <53816049+nhatlong1303@users.noreply.github.com>
2022-05-24 15:31:32 +0700

    VNDC mobile - dark mode

--- src/components/screens/Futures/TradeRecord/index.js
+++ src/components/screens/Futures/TradeRecord/index.js
@@ -99,7 +99,7 @@ const FuturesTradeRecord = ({ isVndcFutures, layoutConfig, pairConfig, pairPrice
                     onClick={hideOtherToggle}
                 >
                     <CheckBox active={hideOther} />{' '}
-                    <span className='ml-1 whitespace-nowrap text-gray font-medium capitalize'>
+                    <span className='ml-1 whitespace-nowrap text-gray dark:text-txtSecondary-dark font-medium capitalize'>
                         {t('futures:hide_other_symbols')}
                     </span>
                 </div>
