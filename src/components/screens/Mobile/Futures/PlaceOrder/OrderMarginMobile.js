cd7847a621820d743b95eb3ad7686fda6ee680b7
trungnd <trungnd@attlas.io>
2022-06-02 17:17:39 +0700

    Fix withdraw

--- src/components/screens/Mobile/Futures/PlaceOrder/OrderMarginMobile.js
+++ src/components/screens/Mobile/Futures/PlaceOrder/OrderMarginMobile.js
@@ -11,17 +11,20 @@ const OrderMarginMobile = ({ marginAndValue, pairConfig, availableAsset }) => {
     const quoteAsset = pairConfig?.quoteAsset ?? '';
 
     const openTransferModal = () => {
-        dispatch(setTransferModal({ isVisible: true, asset: quoteAsset }))
+        // dispatch(setTransferModal({ isVisible: true, asset: quoteAsset }))
+        window.ReactNativeWebView && window.ReactNativeWebView.postMessage('deposit');
     }
 
     const Available = () => {
         return (
             <div className="flex items-center flex-wrap">
-                {formatNumber(availableAsset * 100000000 ?? 0, 0)}&nbsp;
-                {/*<img src={getS3Url('/images/icon/ic_add.png')} height={16} width={16} className='min-w-[16px]' onClick={openTransferModal} />*/}
+                {formatNumber(availableAsset * 100000000 ?? 0, 0)}&nbsp;&nbsp;
+                <img src={getS3Url('/images/icon/ic_add.png')} height={16} width={16} className='min-w-[16px]' onClick={openTransferModal} />
             </div>
         )
     }
+
+    const onMobile = !!window?.ReactNativeWebView
 
     return (
         <div className="flex flex-col h-full justify-around">
@@ -29,7 +32,8 @@ const OrderMarginMobile = ({ marginAndValue, pairConfig, availableAsset }) => {
                 <div className="mr-1 text-txtSecondary dark:text-txtSecondary-dark min-w-[50px]">{t('futures:mobile:available')}</div>
                 <div className="flex items-end justify-end text-right" style={{ wordBreak: 'break-word' }}>
                     {formatNumber(availableAsset ?? 0, 0)}
-                    {/*<img src={getS3Url('/images/icon/ic_add.png')} height={16} width={16} className='min-w-[16px]' onClick={openTransferModal} />*/}
+                    {/*{onMobile && <> &nbsp;&nbsp;<img src={getS3Url('/images/icon/ic_add.png')} height={16} width={16} className='min-w-[16px]' onClick={openTransferModal} /></>}*/}
+                    <> &nbsp;&nbsp;<img src={getS3Url('/images/icon/ic_add.png')} height={16} width={16} className='min-w-[16px]' onClick={openTransferModal} /></>
                 </div>
             </div>
             <div className="flex justify-between text-xs font-medium ">
