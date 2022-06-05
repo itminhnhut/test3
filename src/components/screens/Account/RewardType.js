1e20dcf1582a06be56d73d7d64a27151c132d937
nhatlong1303 <53816049+nhatlong1303@users.noreply.github.com>
2022-06-05 23:05:55 +0700

    feature - event - update content

--- src/components/screens/Account/RewardType.js
+++ src/components/screens/Account/RewardType.js
@@ -1,6 +1,6 @@
 import { memo, useCallback, useEffect, useState } from 'react';
 import { useTranslation } from 'next-i18next';
-import { formatNumber, formatTime, getLoginUrl, getS3Url } from 'redux/actions/utils';
+import { formatNumber, formatTime, getLoginUrl, getS3Url, formatCurrency } from 'redux/actions/utils';
 
 import RewardButton, { REWARD_BUTTON_STATUS } from 'components/screens/Account/RewardButton';
 import useIsFirstRender from 'hooks/useIsFirstRender';
@@ -47,12 +47,12 @@ const RewardType = memo(({ data, active, assetConfig, claim, claiming, onClaim }
 
                 return (
                     <div style={width >= BREAK_POINTS.lg ? { width: 'calc(100% - 250px)' } : undefined}
-                         className="mt-3 mb-4 text-xs sm:text-sm">
+                        className="mt-3 mb-4 text-xs sm:text-sm">
                         <div className={'flex items-center ' + textClass}>
                             {stepIcon &&
-                            <img src={getS3Url(stepIcon)}
-                                 className="w-[16px] h-[16px] sm:w-[20px] sm:h-[20px] mr-1.5"
-                                 alt={null}/>}
+                                <img src={getS3Url(stepIcon)}
+                                    className="w-[16px] h-[16px] sm:w-[20px] sm:h-[20px] mr-1.5"
+                                    alt={null} />}
                             <span>{text}</span>
                         </div>
                         <div className="w-full mt-1.5 text-xs lg:text-sm">
@@ -62,23 +62,24 @@ const RewardType = memo(({ data, active, assetConfig, claim, claiming, onClaim }
                 )
             case Types.TASK_CONDITION.PROGRESS:
                 let reached = data?.user_metadata?.metadata?.value || 0
-                const target = data?.metadata?.target
+                let target = data?.metadata?.target;
 
-                if (reached > target) reached = target
-
+                if (reached > target) target = Math.ceil(reached / target) * target;
                 return (
                     <div className="my-4 w-full xl:min-w-[40%] font-medium">
                         <div className="flex items-center">
                             <div className="w-1/2 sm:w-2/3 md:w-3/4 lg:w-1/2 relative rounded-lg overflow-hidden h-[3.65px] lg:h-[5px] xl:h-[8px] bg-teal-lightTeal dark:bg-teal-opacity">
                                 <div style={{ width: reachedProgress + '%' }}
-                                     className="absolute h-full top-0 left-0 bg-dominant rounded-lg transition-all duration-200 ease-in"/>
+                                    className="absolute h-full top-0 left-0 bg-dominant rounded-lg transition-all duration-200 ease-in" />
                             </div>
-                            <div className="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/2 text-right text-xs md:text-sm xl:text-[16px] lg:pr-6 xl:pr-0 xl:ml-8">
+                            <div className="flex text-right text-xs md:text-sm xl:text-[16px] xl:pr-0 xl:ml-8">
                                 <span className="text-dominant">
-                                    {formatNumber(reached, assetConfig?.assetDigit)}
+                                    {formatCurrency(reached, 3)}
                                 </span>
-                                /<span>{formatNumber(target, assetConfig?.assetDigit)}</span>
-                            </div>
+                                <span>/</span>
+                                <span className="break-normal">{formatCurrency(target, 3)}</span>
+                            </div>&nbsp;
+                            <div className="text-xs md:text-sm xl:text-[16px] whitespace-nowrap	">({(data?.user_metadata?.reward?.num_of_rewards ?? 0) + ' ' + data?.reward?.unit[language]})</div>
                         </div>
                         <div className="w-full mt-1.5 text-xs lg:text-sm font-normal">
                             ({t('common:end_at')} {formatTime(data?.ended_at, 'HH:mm dd-MM-yyyy')})
@@ -103,10 +104,10 @@ const RewardType = memo(({ data, active, assetConfig, claim, claiming, onClaim }
             if (action?.type === Types.TASK_CTA_TYPE.URL) {
                 buttonGroup.push(
                     <RewardButton key={`reward_button_${index}`} title={rewardProps?.title}
-                                  status={rewardProps?.status}
-                                  href={rewardProps?.href}
-                                  componentType="a"
-                                  buttonStyles="w-[47%] max-w-[47%] sm:max-w-[120px] xl:min-w-[90px] xl:w-[120px] mr-3"
+                        status={rewardProps?.status}
+                        href={rewardProps?.href}
+                        componentType="a"
+                        buttonStyles="w-[47%] max-w-[47%] sm:max-w-[120px] min-w-[90px] xl:w-[120px] mr-3"
                     />
                 )
             }
@@ -118,7 +119,7 @@ const RewardType = memo(({ data, active, assetConfig, claim, claiming, onClaim }
         }
 
         if (data?.user_metadata?.claim_status === Types.TASK_HISTORY_CLAIM_STATUS.PENDING &&
-            data?.user_metadata?.status === Types.TASK_HISTORY_STATUS.FINISHED)   {
+            data?.user_metadata?.status === Types.TASK_HISTORY_STATUS.FINISHED) {
             claimProps.title = t('reward-center:claim')
             claimProps.status = REWARD_BUTTON_STATUS.AVAILABLE
         } else if (isClaimed) {
@@ -133,20 +134,20 @@ const RewardType = memo(({ data, active, assetConfig, claim, claiming, onClaim }
 
         buttonGroup.push(
             <RewardButton key="reward_button_claim"
-                          onClick={() => claimProps?.status === REWARD_BUTTON_STATUS.AVAILABLE
-                              && onClaim(data?.user_metadata?._id, { reward: data?.reward, assetConfig, category: data?.category })}
-                          title={claiming ? <PulseLoader size={3} color={colors.teal}/> : claimProps?.title}
-                          status={claimProps?.status}
-                          buttonStyles="w-[47%] max-w-[47%] sm:max-w-[120px] xl:min-w-[90px] xl:w-[120px]"
+                onClick={() => claimProps?.status === REWARD_BUTTON_STATUS.AVAILABLE
+                    && onClaim(data?.user_metadata?._id, { reward: data?.reward, assetConfig, category: data?.category })}
+                title={claiming ? <PulseLoader size={3} color={colors.teal} /> : claimProps?.title}
+                status={claimProps?.status}
+                buttonStyles="w-[47%] max-w-[47%] sm:max-w-[120px] min-w-[90px] xl:w-[120px]"
             />
         )
 
         return (
-            <div className="flex items-center lg:justify-end lg:min-w-[200px] xl:min-w-[350px]">
+            <div className="flex items-center lg:justify-end ">
                 {!auth ?
                     <RewardButton title={t('common:sign_in')} buttonStyles="mt-3 w-full min-w-[90px] sm:max-w-[120px]"
-                                  componentType="a" href={getLoginUrl('sso', 'login')}
-                                  status={REWARD_BUTTON_STATUS.AVAILABLE}
+                        componentType="a" href={getLoginUrl('sso', 'login')}
+                        status={REWARD_BUTTON_STATUS.AVAILABLE}
                     />
                     : buttonGroup}
             </div>
@@ -155,9 +156,8 @@ const RewardType = memo(({ data, active, assetConfig, claim, claiming, onClaim }
 
     useEffect(() => {
         let reached = data?.user_metadata?.metadata?.value || 0
-        const target = data?.metadata?.target
-
-        if (reached > target) reached = target
+        let target = data?.metadata?.target;
+        if (reached > target) target = Math.ceil(reached / target) * target;
         const progress = Math.floor(reached / target * 100)
 
         if (active) {
