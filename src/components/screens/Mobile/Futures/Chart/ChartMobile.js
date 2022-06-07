fa3a3e79ac6dca7df123e3f497d202250086c252
phudq <phudq@nami.trade>
2022-06-07 13:45:41 +0700

    temp trading view

--- src/components/screens/Mobile/Futures/Chart/ChartMobile.js
+++ src/components/screens/Mobile/Futures/Chart/ChartMobile.js
@@ -6,6 +6,7 @@ import ChartTimer from './ChartTimer'
 import Indicators from './Indicators'
 import { IconStarFilled } from "components/common/Icons";
 import colors from "styles/colors";
+import FuturesChart from "components/screens/Futures/FuturesChart";
 
 const ChartMobile = memo(({ pairConfig, pair, isVndcFutures, setCollapse, collapse, forceRender, isFullScreen, decimals }) => {
     const [resolution, setResolution] = useState(ms('1d'))
@@ -22,28 +23,34 @@ const ChartMobile = memo(({ pairConfig, pair, isVndcFutures, setCollapse, collap
     return (
         <div style={style} >
             <div className='flex flex-col w-full h-full' >
-                <ChartTimer pair={pair} pairConfig={pairConfig}
-                    isVndcFutures={isVndcFutures} resolution={resolution}
-                    setResolution={setResolution}
-                    candle={candle} setCandle={setCandle}
-                    className="h-[56px]"
-                    isFullScreen={isFullScreen}
-                />
+                {/*<ChartTimer pair={pair} pairConfig={pairConfig}*/}
+                {/*    isVndcFutures={isVndcFutures} resolution={resolution}*/}
+                {/*    setResolution={setResolution}*/}
+                {/*    candle={candle} setCandle={setCandle}*/}
+                {/*    className="h-[56px]"*/}
+                {/*    isFullScreen={isFullScreen}*/}
+                {/*/>*/}
                 <div style={{ height: 'calc(100% - 64px)' }}>
-                    <KlineChart
-                        symbolInfo={{ exchange: 'NAMI_FUTURES', symbol: pair, pricePrecision: decimals?.decimalScalePrice ?? 0 }}
-                        resolution={resolution}
-                        mainIndicator={mainIndicator}
-                        subIndicator={subIndicator}
-                        candle={candle}
-                        isResize={collapse}
-                        forceRender={forceRender}
+                    <FuturesChart
+                        pair={pairConfig?.pair}
+                        initTimeFrame="1D"
+                        isVndcFutures={true}
+                        ordersList={[]}
                     />
+                    {/*<KlineChart*/}
+                    {/*    symbolInfo={{ exchange: 'NAMI_FUTURES', symbol: pair, pricePrecision: decimals?.decimalScalePrice ?? 0 }}*/}
+                    {/*    resolution={resolution}*/}
+                    {/*    mainIndicator={mainIndicator}*/}
+                    {/*    subIndicator={subIndicator}*/}
+                    {/*    candle={candle}*/}
+                    {/*    isResize={collapse}*/}
+                    {/*    forceRender={forceRender}*/}
+                    {/*/>*/}
                 </div>
-                <Indicators
-                    setCollapse={setCollapse} collapse={collapse}
-                    setMainIndicator={setMainIndicator} mainIndicator={mainIndicator}
-                    setSubIndicator={setSubIndicator} subIndicator={subIndicator} />
+                {/*<Indicators*/}
+                {/*    setCollapse={setCollapse} collapse={collapse}*/}
+                {/*    setMainIndicator={setMainIndicator} mainIndicator={mainIndicator}*/}
+                {/*    setSubIndicator={setSubIndicator} subIndicator={subIndicator} />*/}
             </div>
         </div>
     );
