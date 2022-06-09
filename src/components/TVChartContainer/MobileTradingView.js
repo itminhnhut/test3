215bccea3bbd1c346aba162817bd44e1002b5b2f
phudq <phudq@nami.trade>
2022-06-09 09:25:05 +0700

    chore(order-detail): hide some info

--- src/components/TVChartContainer/MobileTradingView.js
+++ src/components/TVChartContainer/MobileTradingView.js
@@ -466,8 +466,8 @@ MobileTradingView.defaultProps = {
     clientId: "tradingview.com",
     userId: "public_user_id",
     fullscreen: false,
-    showSymbol: false,
-    showIconGuide: false,
+    showSymbol: true,
+    showIconGuide: true,
     autosize: true,
     time_frames: [{text: "1h", resolution: "60", description: "1h"}],
     studies_overrides: {
