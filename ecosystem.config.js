ea4d5cb4c652e772e8c0b084cbc1a8f16bd17361
trungnd <trungnd@attlas.io>
2022-06-14 15:25:12 +0700

    Fix build script

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,20 @@
 module.exports = {
-    apps: [{
-        name: 'nami-exchange-web-v2',
-        script: 'yarn',
-        args: 'start',
-        interpreter: '/bin/bash',
-        max_memory_restart: '1G',
-        env: {
-            NODE_ENV: 'development',
+    apps: [
+        {
+            name: 'nami-exchange-web-v2',
+            script: 'npm run start',
+            max_memory_restart: '2G',
+            env: {
+                NODE_ENV: 'development',
+            },
+            env_production: {
+                NODE_ENV: 'production',
+            },
         },
-        env_production: {
-            NODE_ENV: 'production',
+    ],
+    deploy: {
+        production: {
+            'post-deploy': 'sh nextjs-pm2-deploy.sh',
         },
-        time: true,
-    }],
+    },
 };
--- nextjs-pm2-deploy.sh
+++ nextjs-pm2-deploy.sh
@@ -1,11 +1,11 @@
 echo "Deploy starting..."
 
-npm install || exit
+npm run install || exit
 
 BUILD_DIR=temp npm run build || exit
 
 if [ ! -d "temp" ]; then
-  echo '\033[31m temp Directory not exists!\033[0m'
+  echo '\033[31m temp Directory not exists!\033[0m'  
   exit 1;
 fi
 
