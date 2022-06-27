dea1f456000c16c88aca5d712ab57b9764d68e9b
trungnd <trungnd@attlas.io>
2022-06-27 15:22:41 +0700

    Fix lang

--- src/components/screens/Mobile/Futures/TabOrders/TabTransactionsHistory.js
+++ src/components/screens/Mobile/Futures/TabOrders/TabTransactionsHistory.js
@@ -1,37 +1,40 @@
-import {endOfDay, format, startOfDay} from "date-fns";
-import {Check, ChevronDown, ChevronUp, Copy} from "react-feather";
+import { endOfDay, format, startOfDay } from "date-fns";
+import { Check, ChevronDown, ChevronUp, Copy } from "react-feather";
 import colors from "styles/colors";
 import DateRangePicker from "components/screens/Mobile/Futures/DateRangePicker";
-import React, {useEffect, useMemo, useState} from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import Modal from "components/common/ReModal";
 import fetchApi from 'utils/fetch-api';
-import {API_GET_VNDC_FUTURES_TRANSACTION_HISTORIES} from "redux/actions/apis";
+import { API_GET_VNDC_FUTURES_TRANSACTION_HISTORIES } from "redux/actions/apis";
 import classNames from "classnames";
-import {concat, filter, find, first, keyBy, last, range} from "lodash";
+import { concat, filter, find, first, keyBy, last, range } from "lodash";
 import InfiniteScroll from "react-infinite-scroll-component";
-import {IconLoading} from "components/common/Icons";
+import { IconLoading } from "components/common/Icons";
 import AssetLogo from "components/wallet/AssetLogo";
-import {useSelector} from "react-redux";
-import {formatNumber} from "redux/actions/utils";
-import {useTranslation} from "next-i18next";
-import {CopyToClipboard} from "react-copy-to-clipboard/lib/Component";
+import { useSelector } from "react-redux";
+import { formatNumber } from "redux/actions/utils";
+import { useTranslation } from "next-i18next";
+import { CopyToClipboard } from "react-copy-to-clipboard/lib/Component";
 import TableNoData from "components/common/table.old/TableNoData";
+import { TransactionCategory } from "../../../../../redux/actions/const";
 
 const categories = [
     'all',// "All",
-    600,// "Fee",
-    602,// "Profit",
-    606,// "Insurance Clearance Fee",
-    603,// "Funding Rates",
-    4,// "Deposit",
-    723,// "Withdraw",
-    608,// "Insurance",
-    609,// "Promotion"
+    TransactionCategory.FUTURE_PLACE_ORDER_FEE, // 600
+    TransactionCategory.FUTURE_CLOSE_ORDER_PROFIT, // 602
+    TransactionCategory.FUTURE_SWAP, // 603
+    TransactionCategory.FUTURE_VNDC_LIQUIDATE_FEE, // 606
+    TransactionCategory.FUTURE_INSURANCE_FUND, // 608
+    TransactionCategory.FUTURE_PROMOTION, // 609
+    TransactionCategory.DEPOSIT, //T: 4
+    TransactionCategory.VNDC_DIRECT_WITHDRAW, // 723
+
 ]
 
 const ASSETS = [72]
 
-function TabTransactionsHistory({scrollSnap, active}) {
+
+function TabTransactionsHistory({ scrollSnap, active }) {
     const [data, setData] = useState({
         result: [],
         hasNext: false
@@ -47,7 +50,7 @@ function TabTransactionsHistory({scrollSnap, active}) {
     const assetConfigs = useSelector(state => state.utils.assetConfig)
     const timestamp = useSelector((state) => state.heath.timestamp);
 
-    const {t} = useTranslation()
+    const { t } = useTranslation()
 
     const assetConfigMap = useMemo(() => {
         return keyBy(filter(assetConfigs, (a) => ASSETS.includes(a.id)), 'id')
@@ -90,6 +93,18 @@ function TabTransactionsHistory({scrollSnap, active}) {
         }
     }, [range, category, active, timestamp])
 
+    const _renderCategory = (item) => {
+        if(!item) return '-'
+        const note = (item.note).toLowerCase()
+        if (item.category === TransactionCategory.FUTURE_PLACE_ORDER_FEE) {
+    
+            return note.includes('close')
+                ? t(`futures:mobile:transaction_histories:categories:close_fee`)
+                : t(`futures:mobile:transaction_histories:categories:open_fee`)
+        }
+        return categories.includes(item.category) ? t(`futures:mobile:transaction_histories:categories:${item.category}`) : '--'
+    }
+
     const ListData = () => {
         return data.result.map(item => {
             const assetConfig = assetConfigMap[item.currency]
@@ -99,7 +114,7 @@ function TabTransactionsHistory({scrollSnap, active}) {
                 onClick={() => setTransactionDetail(item)}
             >
                 <div className='flex items-center'>
-                    <AssetLogo size={36} assetCode={assetConfig?.assetCode}/>
+                    <AssetLogo size={36} assetCode={assetConfig?.assetCode} />
                     <div className='ml-2'>
                         <div className='font-bold text-onus-white'>{assetConfig?.assetCode}</div>
                         <div
@@ -118,7 +133,7 @@ function TabTransactionsHistory({scrollSnap, active}) {
                     <div
                         className='font-medium text-onus-grey text-xs'
                     >
-                        {categories.includes(item.category) ? t(`futures:mobile:transaction_histories:categories:${item.category}`) : '--'}
+                        {_renderCategory(item)}
                     </div>
                 </div>
             </div>
@@ -158,7 +173,7 @@ function TabTransactionsHistory({scrollSnap, active}) {
                         </> :
                         t('futures:mobile:transaction_histories:time')}
                 </span>
-                <ChevronDown size={12} color={colors.onus.grey}/>
+                <ChevronDown size={12} color={colors.onus.grey} />
             </div>
             <div
                 className={classNames('flex items-center p-2 -mr-2', {
@@ -167,8 +182,8 @@ function TabTransactionsHistory({scrollSnap, active}) {
                 onClick={() => setVisibleCategoryPicker(true)}>
                 <span className='mr-1'>{t(`futures:mobile:transaction_histories:categories:${category}`)}</span>
                 {visibleCategoryPicker ?
-                    <ChevronUp size={12} color={colors.onus.base}/> :
-                    <ChevronDown size={12} color={colors.onus.grey}/>}
+                    <ChevronUp size={12} color={colors.onus.base} /> :
+                    <ChevronDown size={12} color={colors.onus.grey} />}
             </div>
         </div>
         <div
@@ -180,19 +195,19 @@ function TabTransactionsHistory({scrollSnap, active}) {
                 hasMore={data.hasNext && !loading}
                 next={() => fetchData(true)}
                 scrollableTarget="futures-mobile"
-                loader={<div><IconLoading color={colors.onus.white}/></div>}
-                {...scrollSnap ? {height: 'calc(100vh - 5.25rem)'} : {scrollableTarget: "futures-mobile"}}
+                loader={<div><IconLoading color={colors.onus.white} /></div>}
+                {...scrollSnap ? { height: 'calc(100vh - 5.25rem)' } : { scrollableTarget: "futures-mobile" }}
             >
                 {
                     loading ?
-                        <Loading/> :
+                        <Loading /> :
                         !data.result.length ?
                             <TableNoData
                                 isMobile
                                 title={t('futures:order_table:no_transaction_history')}
                                 className="h-[calc(100vh-5.25rem)]"
                             /> :
-                            <ListData/>
+                            <ListData />
                 }
             </InfiniteScroll>
         </div>
@@ -200,7 +215,7 @@ function TabTransactionsHistory({scrollSnap, active}) {
     </div>
 }
 
-const CategoryPicker = ({t, visible, onClose, value, onChange}) => {
+const CategoryPicker = ({ t, visible, onClose, value, onChange }) => {
     const _onChange = (v) => {
         onChange(v)
         onClose()
@@ -220,14 +235,25 @@ const CategoryPicker = ({t, visible, onClose, value, onChange}) => {
                 <span className='text-onus-white py-3 font-medium'>
                     {t(`futures:mobile:transaction_histories:categories:${c}`)}
                 </span>
-                {value === c && <Check size={16} color={colors.onus.base}/>}
+                {value === c && <Check size={16} color={colors.onus.base} />}
             </div>
         })}
     </Modal>
 }
 
-const TransactionDetail = ({t, visible, onClose, transaction, assetConfig = {}}) => {
+const TransactionDetail = ({ t, visible, onClose, transaction, assetConfig = {} }) => {
     const orderId = first(transaction?.note?.match(/\d+/g)) || '--'
+    const _renderCategory = (item) => {
+        if(!item) return '-'
+        const note = (item.note).toLowerCase()
+        if (item.category === TransactionCategory.FUTURE_PLACE_ORDER_FEE) {
+    
+            return note.includes('close')
+                ? t(`futures:mobile:transaction_histories:categories:close_fee`)
+                : t(`futures:mobile:transaction_histories:categories:open_fee`)
+        }
+        return categories.includes(item.category) ? t(`futures:mobile:transaction_histories:categories:${item.category}`) : '--'
+    }
     return <Modal
         isVisible={visible}
         onusMode
@@ -236,7 +262,8 @@ const TransactionDetail = ({t, visible, onClose, transaction, assetConfig = {}})
     >
         <div className='text-onus-white text-center border-b border-onus-bg2 pb-6'>
             <span className='text-sm'>
-                {transaction?.category ? t(`futures:mobile:transaction_histories:categories:${transaction?.category}`) : '--'}
+                {_renderCategory(transaction)}
+                {/* {transaction?.category ? t(`futures:mobile:transaction_histories:categories:${transaction?.category}`) : '--'} */}
             </span>
             <div className='text-2xl font-bold'>
                 <span>{formatNumber(transaction?.money_use, assetConfig.assetDigit, null, true)}</span>
@@ -250,18 +277,18 @@ const TransactionDetail = ({t, visible, onClose, transaction, assetConfig = {}})
                 </span>
                 <div className='flex flex-1 min-w-0 items-center'>
                     <div className='flex-1 min-w-0 overflow-hidden text-right'
-                         style={{textOverflow: 'ellipsis'}}>{transaction?._id}</div>
+                        style={{ textOverflow: 'ellipsis' }}>{transaction?._id}</div>
                     <CopyToClipboard text={transaction?._id}>
-                        <Copy className='ml-2' size={14} color={colors.onus.grey}/>
+                        <Copy className='ml-2' size={14} color={colors.onus.grey} />
                     </CopyToClipboard>
                 </div>
             </div>
             <div className='flex justify-between text-sm'>
                 <span className='text-onus-grey'>{t('futures:mobile:transaction_histories:order_id')}</span>
                 <div className='flex flex-1 min-w-0 items-center'>
-                   <div className='flex-1 min-w-0 overflow-hidden text-right'>{orderId}</div>
+                    <div className='flex-1 min-w-0 overflow-hidden text-right'>{orderId}</div>
                     <CopyToClipboard text={orderId}>
-                        <Copy className='ml-2' size={14} color={colors.onus.grey}/>
+                        <Copy className='ml-2' size={14} color={colors.onus.grey} />
                     </CopyToClipboard>
                 </div>
             </div>
@@ -281,15 +308,15 @@ const Loading = () => {
     return range(0, 10).map(i => {
         return <div key={i} className='flex justify-between animate-pulse p-4 border-b border-onus-line'>
             <div className='flex items-center'>
-                <div className='!rounded-full bg-darkBlue-3 h-9 !w-9'/>
+                <div className='!rounded-full bg-darkBlue-3 h-9 !w-9' />
                 <div className='flex flex-col justify-between h-full ml-2'>
-                    <div className='rounded bg-darkBlue-3 h-3 w-12'/>
-                    <div className='rounded bg-darkBlue-3 h-2 w-28'/>
+                    <div className='rounded bg-darkBlue-3 h-3 w-12' />
+                    <div className='rounded bg-darkBlue-3 h-2 w-28' />
                 </div>
             </div>
             <div className='flex flex-col justify-between items-end'>
-                <div className='rounded bg-darkBlue-3 w-9 h-3'/>
-                <div className='rounded bg-darkBlue-3 w-14 h-2'/>
+                <div className='rounded bg-darkBlue-3 w-9 h-3' />
+                <div className='rounded bg-darkBlue-3 w-14 h-2' />
             </div>
         </div>
     })
