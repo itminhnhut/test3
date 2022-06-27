1078e43320a8563cee494b140cabc4b59d9abb09
nhatlong1303 <53816049+nhatlong1303@users.noreply.github.com>
2022-06-27 17:28:32 +0700

    hotfix - loadmore orderhistory tab

--- src/components/screens/Mobile/Futures/TabOrders/TabOrdersHistory.js
+++ src/components/screens/Mobile/Futures/TabOrders/TabOrdersHistory.js
@@ -10,9 +10,11 @@ import { getShareModalData } from './ShareFutureMobile';
 import { useTranslation } from 'next-i18next';
 import Skeletor from 'components/common/Skeletor';
 import { emitWebViewEvent } from 'redux/actions/utils';
+import { IconLoading } from 'src/components/common/Icons';
+import colors from 'styles/colors'
 
-const TabOrdersHistory = ({isDark, scrollSnap, pair, isVndcFutures, active, onShowDetail}) => {
-    const {t} = useTranslation();
+const TabOrdersHistory = ({ isDark, scrollSnap, pair, isVndcFutures, active, onShowDetail }) => {
+    const { t } = useTranslation();
     const allPairConfigs = useSelector((state) => state?.futures?.pairConfigs);
     const timestamp = useSelector((state) => state.heath.timestamp);
     const [dataSource, setDataSource] = useState([]);
@@ -24,6 +26,7 @@ const TabOrdersHistory = ({isDark, scrollSnap, pair, isVndcFutures, active, onSh
     const rowData = useRef(null);
     const [openShareModal, setOpenShareModal] = useState(false);
     const isLoading = useRef(true);
+    const [loadMore, setLoadMore] = useState(false);
 
     useEffect(() => {
         if (active) {
@@ -39,7 +42,7 @@ const TabOrdersHistory = ({isDark, scrollSnap, pair, isVndcFutures, active, onSh
     const onShowModal = (item, key) => {
         rowData.current = item;
         if (!openShareModal) {
-            const shareModalData = getShareModalData({order: rowData.current, pairPrice: pairConfigDetail})
+            const shareModalData = getShareModalData({ order: rowData.current, pairPrice: pairConfigDetail })
             emitWebViewEvent(JSON.stringify(shareModalData))
         }
         // setOpenShareModal(!openShareModal)
@@ -47,9 +50,9 @@ const TabOrdersHistory = ({isDark, scrollSnap, pair, isVndcFutures, active, onSh
 
     const getOrders = async (isFirstLoad) => {
         try {
-            const {status, data} = await fetchApi({
+            const { status, data } = await fetchApi({
                 url: API_GET_FUTURES_ORDER_HISTORY,
-                options: {method: 'GET'},
+                options: { method: 'GET' },
                 params: {
                     ...filter.current,
                     status: 1,
@@ -67,11 +70,13 @@ const TabOrdersHistory = ({isDark, scrollSnap, pair, isVndcFutures, active, onSh
         } finally {
             isLoading.current = false;
             setLoading(false);
+            setLoadMore(false)
         }
     }
 
     const onNext = () => {
         filter.current.page = filter.current.page + 1;
+        setLoadMore(true);
         getOrders();
     }
 
@@ -86,70 +91,78 @@ const TabOrdersHistory = ({isDark, scrollSnap, pair, isVndcFutures, active, onSh
                 <div className="flex items-center">
                     <div className="flex flex-col">
                         <div className="flex">
-                            <Skeletor width={100} height={21}/>
+                            <Skeletor width={100} height={21} />
                         </div>
                         <div className="flex">
-                            <Skeletor width={50} height={10}/>&nbsp;&nbsp;
-                            <Skeletor width={50} height={10}/>
+                            <Skeletor width={50} height={10} />&nbsp;&nbsp;
+                            <Skeletor width={50} height={10} />
                         </div>
                     </div>
                 </div>
                 <div className="flex">
                     <div>
-                        <Skeletor width={80} height={8}/>
-                        <Skeletor width={80} height={8}/>
+                        <Skeletor width={80} height={8} />
+                        <Skeletor width={80} height={8} />
                     </div>
-                    <div className="ml-[16px]"><Skeletor width={30} height={30}/></div>
+                    <div className="ml-[16px]"><Skeletor width={30} height={30} /></div>
                 </div>
             </div>
             <div className="flex items-center justify-between flex-wrap mt-[5px]">
-                <div className="w-[48%]"><Skeletor width={'100%'} height={10}/></div>
-                <div className="w-[48%]"><Skeletor width={'100%'} height={10}/></div>
-                <div className="w-[48%]"><Skeletor width={'100%'} height={10}/></div>
-                <div className="w-[48%]"><Skeletor width={'100%'} height={10}/></div>
-                <div className="w-[48%]"><Skeletor width={'100%'} height={10}/></div>
-                <div className="w-[48%]"><Skeletor width={'100%'} height={10}/></div>
-                <div className="w-[48%]"><Skeletor width={'100%'} height={10}/></div>
-                <div className="w-[48%]"><Skeletor width={'100%'} height={10}/></div>
+                <div className="w-[48%]"><Skeletor width={'100%'} height={10} /></div>
+                <div className="w-[48%]"><Skeletor width={'100%'} height={10} /></div>
+                <div className="w-[48%]"><Skeletor width={'100%'} height={10} /></div>
+                <div className="w-[48%]"><Skeletor width={'100%'} height={10} /></div>
+                <div className="w-[48%]"><Skeletor width={'100%'} height={10} /></div>
+                <div className="w-[48%]"><Skeletor width={'100%'} height={10} /></div>
+                <div className="w-[48%]"><Skeletor width={'100%'} height={10} /></div>
+                <div className="w-[48%]"><Skeletor width={'100%'} height={10} /></div>
             </div>
-            <div className="h-[1px]"><Skeletor width={'100%'} height={1} className="mt-[15px]"/></div>
+            <div className="h-[1px]"><Skeletor width={'100%'} height={1} className="mt-[15px]" /></div>
         </div>
     )
 
     const getLoading = () => {
         let rs = [];
         for (let i = 1; i <= 5; i++) {
-            rs.push(<Loading key={i}/>)
+            rs.push(<Loading key={i} />)
         }
         return rs;
     }
 
     if (loading && isLoading.current) return (<div className="min-h-screen px-[10px]">{getLoading()}</div>)
     if (dataSource.length <= 0 && !loading) return <TableNoData isMobile
-                                                                title={t('futures:order_table:no_history_order')}
-                                                                className="h-full min-h-screen"/>
+        title={t('futures:order_table:no_history_order')}
+        className="h-full min-h-screen" />
 
     return (
         <div className="min-h-screen">
-            <InfiniteScroll
-                dataLength={dataSource.length}
-                next={onNext}
-                hasMore={active && hasMore.current}
-                {...scrollSnap ? {height: 'calc(100vh - 42px)'} : {scrollableTarget: "futures-mobile"}}
-                // loader={loading ? loader() : null}
-            >
+            <div className={scrollSnap ? 'h-[calc(100vh-42px)] overflow-y-auto' : ''}>
                 <div className="px-[16px]">
                     {dataSource?.map((order, i) => {
                         const symbol = allPairConfigs.find(rs => rs.symbol === order.symbol);
                         return (
                             <OrderItemMobile key={i} order={order} mode="history"
-                                             isDark={isDark} onShowDetail={onShowDetail} symbol={symbol}
-                                             onShowModal={onShowModal}
+                                isDark={isDark} onShowDetail={onShowDetail} symbol={symbol}
+                                onShowModal={onShowModal}
                             />
                         )
                     })}
+                    {active && hasMore.current && <div
+                        className='flex items-center justify-center text-center h-10 text-sm font-semibold'
+                        onClick={onNext}
+                    >{loadMore ? <IconLoading color={colors.onus.white} /> : <span>Load more</span>}
+                    </div>}
                 </div>
-            </InfiniteScroll>
+            </div>
+            {/* <InfiniteScroll
+                dataLength={dataSource.length}
+                next={onNext}
+                hasMore={active && hasMore.current}
+                {...scrollSnap ? { height: 'calc(100vh - 42px)' } : { scrollableTarget: "futures-mobile" }}
+            // loader={loading ? loader() : null}
+            >
+
+            </InfiniteScroll> */}
         </div>
     );
 };
