73708e21acfad031e82f251a27df719e4085073a
trungnd <trungnd@attlas.io>
2022-06-29 14:46:47 +0700

    Fix language

--- src/components/screens/Nao/Section/NaoPerformance.js
+++ src/components/screens/Nao/Section/NaoPerformance.js
@@ -116,7 +116,7 @@ const NaoPerformance = () => {
                     </div>
                 </CardNao>
                 <CardNao>
-                    <label className="text-nao-text font-medium sm:text-lg">{t('nao:onus_performance:total_traders')}</label>
+                    <label className="text-nao-text font-medium sm:text-lg">{t('nao:onus_performance:total_orders')}</label>
                     <div className="pt-4">
                         <div className="text-nao-white text-[1.375rem] font-semibold pb-2">{dataSource ? formatNumber(dataSource?.count, 0) : '-'}</div>
                         <span className="text-sm text-nao-grey capitalize">{dataSource ? formatNumber(dataSource?.userCount, 0) + ' ' + t('nao:onus_performance:users') : '-'}</span>
