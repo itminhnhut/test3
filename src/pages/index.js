7795907d997cddad9f6ad0c335a236f8e245e89c
trungnd <trungnd@attlas.io>
2022-07-03 21:47:23 +0700

    Rollback

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,64 +1,80 @@
-import React from 'react';
-import styled from 'styled-components';
+import HomeIntroduce from 'src/components/screens/Home/HomeIntroduce';
+import HomeMarketTrend from 'src/components/screens/Home/HomeMarketTrend';
+import HomeAdditional from 'src/components/screens/Home/HomeAdditional';
+import MaldivesLayout from 'src/components/common/layouts/MaldivesLayout';
+import HomeNews from 'src/components/screens/Home/HomeNews';
+import Modal from 'src/components/common/ReModal';
+
+import { useCallback, useState } from 'react';
+import { useTranslation } from 'next-i18next';
+import { QRCode } from 'react-qrcode-logo';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
-import LayoutNaoToken from 'components/common/layouts/LayoutNaoToken'
-import NaoHeader from 'components/screens/Nao/NaoHeader';
-import NaoFooter from 'components/screens/Nao/NaoFooter';
-import NaoInfo from 'components/screens/Nao/Section/NaoInfo';
-import NaoPerformance from 'components/screens/Nao/Section/NaoPerformance';
-import NaoPool from 'components/screens/Nao/Section/NaoPool';
-import NaoToken from 'components/screens/Nao/Section/NaoToken';
-import { getS3Url } from 'redux/actions/utils';
-import { useWindowSize } from 'utils/customHooks';
+import { NAVBAR_USE_TYPE } from 'src/components/common/NavBar/NavBar';
+import Button from 'components/common/Button';
 
-const NaoDashboard = () => {
-    const { width } = useWindowSize();
+const APP_URL = process.env.APP_URL || 'https://nami.exchange'
 
-    const onDownload = (key) => {
-        let url = '';
-        switch (key) {
-            case 'app_store':
-                url = 'https://apps.apple.com/us/app/onus-invest-btc-eth-doge/id1498452975';
-                break;
-            case 'google_play':
-                url = 'https://play.google.com/store/apps/details?id=com.vndc';
-                break;
-            default:
-                break;
-        }
-        window.open(url, '_blank');
-    }
+const Index = () => {
+    // * Initial State
+    const [state, set] = useState({
+        showQR: false,
+    })
+    const setState = (state) => set((prevState) => ({ ...prevState, ...state }))
+
+    // * Use Hooks
+    const { t } = useTranslation(['home', 'modal'])
+
+    // * Render Handler
+    const renderQrCodeModal = useCallback(() => {
+        return (
+            <Modal
+                isVisible={state.showQR}
+                title={t('modal:scan_qr_to_download')}
+                onBackdropCb={() => setState({ showQR: false })}
+            >
+                <div className='flex items-center justify-center'>
+                    <QRCode
+                        value={`${APP_URL}#nami_exchange_download_app`}
+                        size={128}
+                    />
+                </div>
+                <div className='mt-4 w-full flex flex-row items-center justify-between'>
+                    <Button
+                        title={t('common:close')}
+                        type='secondary'
+                        componentType='button'
+                        className='!py-2'
+                        onClick={() => setState({ showQR: false })}
+                    />
+                </div>
+            </Modal>
+        )
+    }, [state.showQR])
 
     return (
-        <LayoutNaoToken>
-            <Background width={width}>
-                <div className="px-4 nao:p-0 max-w-[72.5rem] w-full m-auto !mt-0">
-                    <NaoHeader onDownload={onDownload} />
-                    <div className="nao_section">
-                        <NaoInfo />
-                        <NaoPerformance />
-                        <NaoPool />
-                        <NaoToken onDownload={onDownload} />
-                    </div>
-                </div>
-                <NaoFooter />
-            </Background>
-        </LayoutNaoToken>
-    );
-};
-const Background = styled.div.attrs({
-    className: 'min-w-full min-h-screen flex flex-col justify-between'
-})`
-    background-image:${({ width }) => `url(${getS3Url(`/images/nao/bg-dashboard${width <= 780 ? '_mb' : ''}.png`)})`};
-    background-repeat: no-repeat;     
-    background-size: cover;
-    background-position: center top;
-`
+        <MaldivesLayout navOverComponent navMode={NAVBAR_USE_TYPE.FLUENT}>
+            <div className='homepage'>
+                <HomeIntroduce parentState={setState} />
+                <HomeMarketTrend />
+                <HomeNews />
+                <HomeAdditional parentState={setState} />
+                {renderQrCodeModal()}
+            </div>
+        </MaldivesLayout>
+    )
+}
+
 export const getStaticProps = async ({ locale }) => ({
     props: {
         ...(await serverSideTranslations(locale, [
-            'common', 'nao'
+            'common',
+            'navbar',
+            'home',
+            'modal',
+            'input',
+            'table',
         ])),
     },
 })
-export default NaoDashboard;
+
+export default Index
---
+++ src/pages/nao/index.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import styled from 'styled-components';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import LayoutNaoToken from 'components/common/layouts/LayoutNaoToken'
+import NaoHeader from 'components/screens/Nao/NaoHeader';
+import NaoFooter from 'components/screens/Nao/NaoFooter';
+import NaoInfo from 'components/screens/Nao/Section/NaoInfo';
+import NaoPerformance from 'components/screens/Nao/Section/NaoPerformance';
+import NaoPool from 'components/screens/Nao/Section/NaoPool';
+import NaoToken from 'components/screens/Nao/Section/NaoToken';
+import { getS3Url } from 'redux/actions/utils';
+import { useWindowSize } from 'utils/customHooks';
+
+const NaoDashboard = () => {
+    const { width } = useWindowSize();
+
+    const onDownload = (key) => {
+        let url = '';
+        switch (key) {
+            case 'app_store':
+                url = 'https://apps.apple.com/us/app/onus-invest-btc-eth-doge/id1498452975';
+                break;
+            case 'google_play':
+                url = 'https://play.google.com/store/apps/details?id=com.vndc';
+                break;
+            default:
+                break;
+        }
+        window.open(url, '_blank');
+    }
+
+    return (
+        <LayoutNaoToken>
+            <Background width={width}>
+                <div className="px-4 nao:p-0 max-w-[72.5rem] w-full m-auto !mt-0">
+                    <NaoHeader onDownload={onDownload} />
+                    <div className="nao_section">
+                        <NaoInfo />
+                        <NaoPerformance />
+                        <NaoPool />
+                        <NaoToken onDownload={onDownload} />
+                    </div>
+                </div>
+                <NaoFooter />
+            </Background>
+        </LayoutNaoToken>
+    );
+};
+const Background = styled.div.attrs({
+    className: 'min-w-full min-h-screen flex flex-col justify-between'
+})`
+    background-image:${({ width }) => `url(${getS3Url(`/images/nao/bg-dashboard${width <= 780 ? '_mb' : ''}.png`)})`};
+    background-repeat: no-repeat;     
+    background-size: cover;
+    background-position: center top;
+`
+export const getStaticProps = async ({ locale }) => ({
+    props: {
+        ...(await serverSideTranslations(locale, [
+            'common', 'nao'
+        ])),
+    },
+})
+export default NaoDashboard;
