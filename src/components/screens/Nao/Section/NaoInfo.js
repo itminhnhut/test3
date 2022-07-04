65bbd1e82aed974d82ca53b40cfb1f92ce82741c
trungnd <trungnd@attlas.io>
2022-07-04 09:30:14 +0700

    Remove test session

--- src/components/screens/Nao/Section/NaoInfo.js
+++ src/components/screens/Nao/Section/NaoInfo.js
@@ -21,7 +21,6 @@ const NaoInfo = () => {
                             <label className="text-nao-blue uppercase text-[1.25re] font-semibold">NAO</label>
                             <span className="mx-2">•</span>
                             <div className="font-light text-nao-white capitalize">Nami frame futures</div>
-                            <div className="font-light text-nao-white capitalize">{user?.code}</div>
                         </div>
                     </div>
                 </div>
