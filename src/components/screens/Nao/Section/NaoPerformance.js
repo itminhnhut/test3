f28f41870dd9ef9bc7163fd919861ddae39e783e
nhatlong1303 <53816049+nhatlong1303@users.noreply.github.com>
2022-07-05 17:26:42 +0700

    pool - hotfix:dropdown

--- src/components/screens/Nao/Section/NaoPerformance.js
+++ src/components/screens/Nao/Section/NaoPerformance.js
@@ -150,11 +150,11 @@ const NaoPerformance = memo(() => {
                                         leaveFrom="opacity-100 translate-y-0"
                                         leaveTo="opacity-0 translate-y-1"
                                     >
-                                        <Popover.Panel className="absolute left-0 z-50 bg-nao-bg3 rounded-md mt-1">
+                                        <Popover.Panel className="absolute top-8 left-0 z-50 bg-nao-bg3 rounded-md mt-1">
                                             <div className="px-2 py-[2] min-w-[72px] shadow-onlyLight font-medium text-xs flex flex-col">
                                                 {assets.map((item, index) => (
                                                     <span onClick={() => { setFee(item?.assetCode); close() }} key={index}
-                                                        className={`py-[1px] my-[2px] cursor-pointer ${item?.assetCode === fee ? 'text-nao-text' : 'text-nao-white'}`}>{item?.assetCode}</span>
+                                                        className={`py-[1px] my-[2px] cursor-pointer ${item?.assetCode === fee ? 'text-nao-white' : 'text-nao-text'}`}>{item?.assetCode}</span>
                                                 ))}
                                             </div>
                                         </Popover.Panel>
