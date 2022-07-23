42e0fe0b26ef0a3ae34cf6fef99a0f55fdf0213f
trungnd <trungnd@attlas.io>
2022-07-23 15:02:33 +0700

    Release vote

--- src/pages/nao/vote/[id].js
+++ src/pages/nao/vote/[id].js
@@ -162,7 +162,7 @@ export default function Vote() {
                     setIsShowProposalModal(true);
                 }}
             >
-                {t('nao:vote:vote')}
+                {t('nao:vote:title')}
             </ButtonNao>;
         }
 
