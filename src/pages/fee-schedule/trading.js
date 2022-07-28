a6c110684de6ea7f8682817d5801688d9b84056c
Minh Nguyen Nhat <>
2022-07-28 12:14:01 +0700

    fix spot fee by vip level

--- src/pages/fee-schedule/trading.js
+++ src/pages/fee-schedule/trading.js
@@ -376,7 +376,9 @@ const TradingFee = () => {
                             <div className="flex justify-between sm:block">
                                 <span className="inline-block min-w-[35px] mr-9">Maker</span>
                                 <span>
-                                    {renderUserFeeConfig(FEE_STRUCTURES.EXCHANGE.MAKER_TAKER.MAKER[0], FEE_STRUCTURES.EXCHANGE.MAKER_TAKER.MAKER[1])}
+                                    {state.vipLevel ? 
+                                    renderUserFeeConfig(FEE_TABLE[state.vipLevel].maker_taker_deducted.split(" ")[0].replace("%", ""), FEE_TABLE[state.vipLevel].maker_taker.split(" ")[0].replace("%", "")) : 
+                                    renderUserFeeConfig(FEE_TABLE[0].maker_taker_deducted.split(" ")[0].replace("%", ""), FEE_TABLE[0].maker_taker.split(" ")[0].replace("%", ""))}
                                 </span>
                             </div>
                         </div>
@@ -384,7 +386,9 @@ const TradingFee = () => {
                             <div className="flex justify-between sm:block">
                                 <span className="inline-block min-w-[35px] mr-9">Taker</span>
                                 <span>
-                                    {renderUserFeeConfig(FEE_STRUCTURES.EXCHANGE.MAKER_TAKER.TAKER[0], FEE_STRUCTURES.EXCHANGE.MAKER_TAKER.TAKER[1])}
+                                    {state.vipLevel ? 
+                                    renderUserFeeConfig(FEE_TABLE[state.vipLevel].maker_taker_deducted.split(" ")[2].replace("%", ""), FEE_TABLE[state.vipLevel].maker_taker.split(" ")[2].replace("%", "")) :
+                                    renderUserFeeConfig(FEE_TABLE[0].maker_taker_deducted.split(" ")[2].replace("%", ""), FEE_TABLE[0].maker_taker.split(" ")[2].replace("%", ""))}
                                 </span>
                             </div>
                         </div>
