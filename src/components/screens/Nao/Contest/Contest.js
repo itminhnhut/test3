ed9435cf5ef25bc15d58ab6af4b6cedddab95066
nhatlong1303 <53816049+nhatlong1303@users.noreply.github.com>
2022-08-04 10:50:11 +0700

    NAO - Contest - season2 - hotfix

--- src/components/screens/Nao/Contest/Contest.js
+++ src/components/screens/Nao/Contest/Contest.js
@@ -10,8 +10,8 @@ import CreateTeamModal from 'components/screens/Nao/Contest/season2/CreateTeamMo
 import ConfirmJoiningTeam from 'components/screens/Nao/Contest/season2/ConfirmJoiningTeam';
 
 export const seasons = [
-    { season: '1', start: '2022-07-07T17:00:00.000Z', end: '2022-07-07T17:00:00.000Z', contest_id: 4, title: 'nao:contest:last_season' },
-    { season: '2', start: '2022-08-08:00:00.000Z', end: '2022-31-27T17:00:00.000Z', contest_id: 5, },
+    { season: '1', start: '2022-07-07T17:00:00.000Z', end: '2022-07-07T17:00:00.000Z', contest_id: 4, button: 'nao:contest:last_season', title: 'first' },
+    { season: '2', start: '2022-08-08:00:00.000Z', end: '2022-31-27T17:00:00.000Z', contest_id: 5, title: 'second' },
 ]
 
 const Contest = (props) => {
