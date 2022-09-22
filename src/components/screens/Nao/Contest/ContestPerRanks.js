f05f06a5af2a62ce2421153a0415b53cf4695c9f
nhatlong1303 <53816049+nhatlong1303@users.noreply.github.com>
2022-09-22 16:15:31 +0700

    Contest - season 3 - fix:rank icon

--- src/components/screens/Nao/Contest/ContestPerRanks.js
+++ src/components/screens/Nao/Contest/ContestPerRanks.js
@@ -179,7 +179,7 @@ const ContestPerRanks = ({ previous, contest_id, minVolumeInd, quoteAsset, lastU
                                         <div className="min-w-[31px] text-nao-grey text-sm font-medium">
                                             {loading ? <Skeletor width={24} height={24} circle /> :
                                                 item?.[rank] && item?.[rank] <= 20 ?
-                                                    <img src={getS3Url(`/images/nao/contest/ic_top_${item?.[rank]}.png`)} className="min-w-[24px] min-h-[24px]" width="24" height="24" alt="" />
+                                                    <img src={getS3Url(`/images/nao/contest/ic_top_${index + 4}.png`)} className="min-w-[24px] min-h-[24px]" width="24" height="24" alt="" />
                                                     : item?.[rank] || '-'
                                             }
                                         </div>
