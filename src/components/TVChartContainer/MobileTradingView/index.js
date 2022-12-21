f93e30c391e0d7559f2e4bbc10dabbab835b577f
Minh Nguyen Nhat <>
2022-12-21 14:27:42 +0700

    feat: warning when funding time

--- src/components/TVChartContainer/MobileTradingView/index.js
+++ src/components/TVChartContainer/MobileTradingView/index.js
@@ -664,22 +664,6 @@ export class MobileTradingView extends React.PureComponent {
     }
 }
 
-function getCookie(cname) {
-    let name = cname + "=";
-    let decodedCookie = decodeURIComponent(document.cookie);
-    let ca = decodedCookie.split(';');
-    for (let i = 0; i < ca.length; i++) {
-        let c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
-}
-
 const Funding = ({ symbol }) => {
     const { t } = useTranslation()
     const [showModal, setShowModal] = React.useState(false)
