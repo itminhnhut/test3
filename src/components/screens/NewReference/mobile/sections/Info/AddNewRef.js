ff279a9ca8ceb31e1709f408e9c82d24312b487c
Minh Nguyen Nhat <>
2023-01-06 17:16:28 +0700

    fix: desktop

--- src/components/screens/NewReference/mobile/sections/Info/AddNewRef.js
+++ src/components/screens/NewReference/mobile/sections/Info/AddNewRef.js
@@ -125,43 +125,148 @@ const AddNewRef = ({ isShow = false, onClose, doRefresh, defaultRef, isDesktop }
         if (!resultData.message.length) return null
         const Icon = resultData.isSucess ? <SuccessIcon /> : <ErrorIcon />
         const title = resultData.isSucess ? t('reference:referral.success') : t('reference:referral.error')
-        return <PopupModal
-            isVisible={true}
-            onBackdropCb={onClose}
-            // useAboveAll
-            isDesktop={isDesktop}
-            useCenter={isDesktop}
-            isMobile
-            bgClassName='!z-[400]'
-            containerClassName='!z-[401]'
-        >
-            <div className='w-full flex justify-center items-center flex-col text-center px-2'>
-                <div className='mt-6'>{Icon}</div>
-                <div className='text-gray-6 text-[20px] leading-8 font-semibold mt-6'>
-                    {title}
+        return isDesktop ?
+            <Modal
+                center
+                isVisible
+                containerClassName='!px-6 !py-8 top-[50%]'
+            >
+                <div className='w-full flex justify-center items-center flex-col text-center'>
+                    {Icon}
+                    <div className='text-sm font-medium mt-6'>
+                        <div dangerouslySetInnerHTML={{ __html: resultData.message }} />
+                    </div>
+                    <div className='w-full h-11 flex justify-center items-center bg-teal text-white font-semibold text-sm rounded-md mt-6'
+                        onClick={() => {
+                            setResultData({
+                                isSucess: false,
+                                message: ''
+                            })
+                            resultData.isSucess && doClose()
+                        }}
+                    >
+                        {t('common:confirm')}
+                    </div>
                 </div>
-                <div className='text-sm font-medium mt-3 text-gray-7'>
-                    <div dangerouslySetInnerHTML={{ __html: resultData.message }} />
+            </Modal >
+            :
+            <PopupModal
+                isVisible={true}
+                onBackdropCb={onClose}
+                // useAboveAll
+                isDesktop={isDesktop}
+                useCenter={isDesktop}
+                isMobile
+                bgClassName='!z-[400]'
+                containerClassName='!z-[401]'
+            >
+                <div className='w-full flex justify-center items-center flex-col text-center px-2'>
+                    <div className='mt-6'>{Icon}</div>
+                    <div className='text-gray-6 text-[20px] leading-8 font-semibold mt-6'>
+                        {title}
+                    </div>
+                    <div className='text-sm font-medium mt-3 text-gray-7'>
+                        <div dangerouslySetInnerHTML={{ __html: resultData.message }} />
+                    </div>
+                    <div className='w-full h-11 flex justify-center items-center bg-namiapp-green-1 text-white font-semibold text-sm rounded-md mt-8'
+                        onClick={() => {
+                            setResultData({
+                                isSucess: false,
+                                message: ''
+                            })
+                            resultData.isSucess && doClose()
+                        }}
+                    >
+                        {t('common:confirm')}
+                    </div>
                 </div>
-                <div className='w-full h-11 flex justify-center items-center bg-namiapp-green-1 text-white font-semibold text-sm rounded-md mt-8'
-                    onClick={() => {
-                        setResultData({
-                            isSucess: false,
-                            message: ''
-                        })
-                        resultData.isSucess && doClose()
-                    }}
-                >
-                    {t('common:confirm')}
-                </div>
-            </div>
-        </PopupModal>
+            </PopupModal>
     }, [resultData])
 
     return (
         <>
             {renderResult}
-            <PopupModal
+            {isDesktop ? <PopupModal
+                isVisible={isShow}
+                onBackdropCb={onClose}
+                title={t('reference:referral.add_new_referral')}
+                useAboveAll
+                isDesktop={isDesktop}
+                useCenter={isDesktop}
+                contentClassname={isDesktop ? "!rounded !w-[390px] !px-0" : undefined}
+            >
+                <div className={classNames('font-medium text-sm text-gray-1 leading-6 flex flex-col gap-4', { 'px-4': isDesktop })}>
+                    <div>
+                        {t('reference:referral.commission_rate')}
+                        <div className='mt-4 mb-2'>
+                            <Slider axis='x' x={percent} xmax={totalRate} onChange={onPercentChange} />
+                        </div>
+                        <div className='flex justify-between items-center font-medium text-xs leading-5'>
+                            <div>
+                                {t('reference:referral.you_get')} {totalRate - percent}%
+                            </div>
+                            <div>
+                                {t('reference:referral.friends_get')} {percent}%
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        {t('reference:referral.addref_title')}
+                        <div className={classNames('mt-1 rounded-[4px] px-3 h-11 flex justify-between items-center bg-gray-4 font-medium text-sm leading-6 gap-4', { 'border-red border-[1px]': error.length })}>
+                            <div className='flex w-full justify-between items-center '>
+                                <input id='refCode' className='text-darkBlue font-medium w-full' maxLength={8} placeholder={t('reference:referral.ref_placeholder')} onChange={handleInputRefCode} value={refCode}
+                                    onBlur={() => doCheckRef(refCode)}
+                                    onInput={(e) => {
+                                        handleInput(e, 8)
+                                    }} />
+                                {refCode.length ? <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"
+                                    onClick={() => {
+                                        setRefCode('')
+                                        setError('')
+                                        document.getElementById("refCode").focus();
+                                    }}
+                                >
+                                    <path d="m6 6 12 12M6 18 18 6" stroke='#718096' stroke-linecap="round" stroke-linejoin="round" />
+                                </svg> : null}
+                            </div>
+                            <div className='w-10'>
+                                {refCode.length}/8
+                            </div>
+                        </div>
+                        {error.length ? <div className='mt-1 text-red font-medium text-xs'>
+                            {error}
+                        </div> : null}
+                    </div>
+                    <div>
+                        {t('reference:referral.note')}
+                        <div className='mt-1 rounded-[4px] px-3 h-11 flex justify-between items-center bg-gray-4 font-medium text-sm leading-6 gap-4'>
+                            <div className='w-full justify-between items-center flex'>
+                                <input id='note' className='text-darkBlue font-medium w-full' value={note} maxLength={30} onChange={handleInputNote} onInput={(e) => handleInput(e, 30)} />
+                                {note.length ? <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"
+                                    onClick={() => {
+                                        setNote('')
+                                        document.getElementById("note").focus();
+                                    }}
+                                >
+                                    <path d="m6 6 12 12M6 18 18 6" stroke='#718096' stroke-linecap="round" stroke-linejoin="round" />
+                                </svg> : null}
+                            </div>
+                            <div className='w-10'>
+                                {note.length}/30
+                            </div>
+                        </div>
+                    </div>
+                    <div className='flex items-center gap-2 text-xs font-medium'>
+                        <input type="checkbox" id="vehicle1" className='rounded-sm h-4 w-4 text-darkBlue font-medium' name="isDefault" onChange={handleCheckDefault} checked={isDefault} />
+                        {t('reference:referral.set_default')}
+                    </div>
+                    <div className={classNames('w-full h-11 mt-4 bg-teal rounded-md text-white font-semibold text-sm leading-6 flex items-center justify-center cursor-pointer', { '!bg-gray-3': error.length || (refCode.length && refCode.length !== 8) })}
+                        onClick={error.length || (refCode.length > 0 && refCode.length !== 8) ? null : () => handleAddNewRef()}
+                    >
+                        {t('reference:referral.addref')}
+                    </div>
+                </div>
+            </PopupModal> : <PopupModal
                 isVisible={isShow}
                 onBackdropCb={onClose}
                 title={t('reference:referral.add_new_referral')}
@@ -253,7 +358,7 @@ const AddNewRef = ({ isShow = false, onClose, doRefresh, defaultRef, isDesktop }
                         {t('reference:referral.addref')}
                     </div>
                 </div>
-            </PopupModal>
+            </PopupModal>}
         </>
     )
 }
