7f0813e7bab6ebbd13a5f913b0ebd007b2c0da13
lamnv@nami.trade <lamnv@nami.trade>
2023-02-02 14:23:36 +0700

    done Wallet futures

---
+++ src/components/common/V2/ButtonV2/TextButton.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import classNames from 'classnames';
+
+const TextButton = ({ className = '', disabled = false, children, variants = 'primary', onClick, loading }) => {
+    return (
+        <button
+            className={classNames(
+                'flex items-center justify-center font-semibold text-sm h-[2.75rem] sm:h-[3rem] w-full py-6 bg-transparent',
+                {
+                    'text-teal': variants === 'primary',
+                    'text-gray-5/[0.1]': disabled
+                },
+                className
+            )}
+            onClick={onClick}
+            disabled={disabled || loading}
+        >
+            {children}
+        </button>
+    );
+};
+
+export default TextButton;
