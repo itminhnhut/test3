abbc473ffb03ba4e91183967b7c1e82c7e017152
lamnv@nami.trade <lamnv@nami.trade>
2023-02-08 16:48:54 +0700

    done modal funding fee 1

--- src/components/svg/ChevronDown.js
+++ src/components/svg/ChevronDown.js
@@ -1,6 +1,14 @@
-const SvgChevronDown = ({ color = '#8694B3', size=16, fill, style, className, onClick }) => {
+const SvgChevronDown = ({ color = '#8694B3', size = 16, fill, style, className, onClick }) => {
     return (
-        <svg onClick={onClick} className={`transition ease-in-out rotate-180 ${className}`} width={size} height={size} viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg
+            onClick={onClick}
+            className={`transition ease-in-out rotate-180 ${className}`}
+            width={size}
+            height={size}
+            viewBox="0 0 16 16"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
             <g clip-path="url(#clip0_295_61062)">
                 <path d="M11.3333 9.33337L7.99992 6.00004L4.66659 9.33337L11.3333 9.33337Z" fill={color} />
             </g>
