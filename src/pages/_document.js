2c81890bdc866885e3e72288addec8237bbec265
trungnguyen <trungnd@nami.trade>
2023-02-10 10:59:41 +0700

    fix: enable tag manager

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -36,15 +36,15 @@ class MyDocument extends Document {
                         src='/library/trading_view/datafeeds/udf/dist/bundle.js'
                     />
 
-                    {/* <script */}
-                    {/*     dangerouslySetInnerHTML={{ */}
-                    {/*         __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': */}
-                    {/*     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], */}
-                    {/*     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= */}
-                    {/*     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); */}
-                    {/*     })(window,document,'script','dataLayer','GTM-PTWQXJB');`, */}
-                    {/*     }} */}
-                    {/* /> */}
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                        })(window,document,'script','dataLayer','GTM-PTWQXJB');`,
+                        }}
+                    />
                     <script
                         dangerouslySetInnerHTML={{
                             __html: `
