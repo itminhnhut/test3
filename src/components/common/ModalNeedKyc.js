5f07a693bd16a869260e25f3ecf371dcc65b9f47
lamnv@nami.trade <lamnv@nami.trade>
2023-02-18 16:33:00 +0700

    update: Modal kyc

--- src/components/common/ModalNeedKyc.js
+++ src/components/common/ModalNeedKyc.js
@@ -2,16 +2,18 @@ import HrefButton from 'components/common/V2/ButtonV2/HrefButton';
 import ModalV2 from 'components/common/V2/ModalV2';
 import useLanguage from 'hooks/useLanguage';
 import { useTranslation } from 'next-i18next';
+import { getS3Url } from 'redux/actions/utils';
 
 const ModalNeedKyc = ({ isOpenModalKyc, onBackdropCb }) => {
-    const { t } = useTranslation(['common']);
-
+    const { t } = useTranslation(['common', 'wallet']);
     const [currentLocale] = useLanguage();
 
     return (
         <ModalV2 isVisible={isOpenModalKyc} onBackdropCb={onBackdropCb} className="!max-w-[400px]" wrapClassName="px-6 flex flex-col">
             {/* <ReModal isVisible={isVisible} containerClassName="p-6 max-w-[400px]"> */}
-            <div className="mb-4 font-bold text-lg text-center capitalize">{t('modal:notice')}</div>
+            <img width={124} height={124} src={getS3Url('/images/screen/account/kyc_require.png')} className="mx-auto" />
+
+            <div className="my-4 font-bold text-lg text-center capitalize">{t('wallet:required_kyc')}</div>
             <div className="text-center px-4">{t('wallet:errors.invalid_kyc_status')}</div>
             <div className="mt-6 flex items-center justify-between">
                 <div className="w-[47%]">
