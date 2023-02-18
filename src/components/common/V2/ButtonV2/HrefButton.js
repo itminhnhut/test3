72b8022881079d1aa135f964d537151d9a4640af
lamnv@nami.trade <lamnv@nami.trade>
2023-02-18 16:44:05 +0700

    fix: HrefButton hover text white

--- src/components/common/V2/ButtonV2/HrefButton.js
+++ src/components/common/V2/ButtonV2/HrefButton.js
@@ -11,7 +11,7 @@ const HrefButton = ({ className = '', disabled = false, href = '/', children, va
                 className={classNames(
                     'flex whitespace-nowrap items-center justify-center rounded-md font-semibold text-base w-full py-3 px-6',
                     {
-                        'bg-green-3 hover:bg-green-4 dark:bg-green-2 dark:hover:bg-green-4 text-white': variants === 'primary',
+                        'bg-green-3 hover:bg-green-4 dark:bg-green-2 dark:hover:bg-green-4 text-white hover:text-white': variants === 'primary',
                         'dark:bg-dark-2 dark:hover:bg-hover-dark dark:active:bg-hover-dark dark:text-txtSecondary-dark': variants === 'secondary',
                         'px-0 text-sm text-txtTextBtn hover:text-txtTextBtn-pressed active:text-txtTextBtn-pressed dark:text-txtTextBtn-dark dark:hover:text-txtTextBtn-dark_pressed dark:active:text-txtTextBtn-dark_pressed disabled:text-txtTextBtn-disabled':
                             variants === 'blank',
