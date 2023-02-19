3fe97402cabc7fd9ca8174e4026b35f2dc58fba1
lamnv@nami.trade <lamnv@nami.trade>
2023-02-19 10:58:03 +0700

    fix: 3 status of sort desc/asc/default

--- src/components/screens/Futures/PairList/index.js
+++ src/components/screens/Futures/PairList/index.js
@@ -24,10 +24,9 @@ const FuturesPairList = memo(({ mode, setMode, isAuth, activePairList, onSelectP
     const pairConfigs = useSelector((state) => state.futures.pairConfigs);
 
     // Sort function:
-    const [sortBy, setSortBy] = useState({}); // null = default, 1 => desc, 2 => asc
-    const [pairTicker, setPairTicker] = useState(null);
+    const [sortBy, setSortBy] = useState({}); // undefined = default, true => desc, false => asc
+    // const [pairTicker, setPairTicker] = useState(null);
     const marketWatch = useSelector((state) => state.futures.marketWatch);
-    // console.log('marketWatch', marketWatch);
     const [dataTable, setDataTable] = useState([]);
     useEffect(() => {
         let data =
@@ -47,13 +46,15 @@ const FuturesPairList = memo(({ mode, setMode, isAuth, activePairList, onSelectP
                 item?.priceChangePercent = pairTicker?.priceChangePercent
             }
         });
-
          // sort by field
          if (Object.keys(sortBy)?.length) {
             const _s = Object.entries(sortBy)[0];
-            data = orderBy(data, [( o) => {
-                const value =  o[`${_s[0]}`]
-                return value ? value : _s[1] ? 1000000 : -1000000} ], [`${_s[1] ? 'asc' : 'desc'}`]);
+            if(_s[1] !== undefined) {
+                data = orderBy(data, [(o) => {
+                    const value =  o[`${_s[0]}`]
+                    return value ? value : _s[1] ? 1000000 : -1000000} ], [`${_s[1] ? 'asc' : 'desc'}`]
+                );
+            }
         }
 
         // filter search
@@ -118,11 +119,16 @@ const FuturesPairList = memo(({ mode, setMode, isAuth, activePairList, onSelectP
         [mode, isDark, isAuth]
     );
 
+    const setSorter = (key) => {
+        setSortBy(prev => prev?.[key] === undefined ? { [key]:true } : prev?.[key] ? { [key]:false } : { [key]:undefined })
+    }
+
     return (
         <div
             className={`${
                 !activePairList ? 'hidden' : ''
-            } py-4 min-w-[400px] bg-bgTabInactive dark:bg-bgTabInactive-dark dark:border border-divider dark:border-divider-dark drop-shadow-onlyLight rounded-md ${className}`}
+            } py-4 min-w-[400px] border border-divider dark:border-divider-dark bg-white dark:bg-dark-4
+            shadow-card_light dark:shadow-popover rounded-md ${className}`}
         >
             <div className="max-h-[352px] flex flex-col">
                 <div className="px-4 mb-7">
@@ -146,15 +152,15 @@ const FuturesPairList = memo(({ mode, setMode, isAuth, activePairList, onSelectP
                     }}
                     className="px-4 mt-7 mb-4 flex items-center justify-between font-normal text-xs text-txtSecondary dark:text-txtSecondary-dark"
                 >
-                    <div onClick={() => setSortBy({ [`symbol`]: !sortBy?.[`symbol`] })} style={{ flex: '1 1 0%' }} className="flex justify-start items-center">
+                    <div onClick={() =>setSorter('symbol') } style={{ flex: '1 1 0%' }} className="flex justify-start items-center select-none">
                         {t('common:asset')}
                         <Sorter isUp={sortBy?.[`symbol`]} />
                     </div>
-                    <div onClick={() => setSortBy({ [`lastPrice`]: !sortBy?.[`lastPrice`] })} style={{ flex: '1 1 0%' }} className="flex justify-end items-center">
+                    <div onClick={() =>setSorter('lastPrice') } style={{ flex: '1 1 0%' }} className="flex justify-end items-center select-none">
                         {t('common:last_price')}
                         <Sorter isUp={sortBy?.[`lastPrice`]} />
                     </div>
-                    <div onClick={() => setSortBy({ [`priceChangePercent`]: !sortBy?.[`priceChangePercent`] })} style={{ flex: '1 1 0%' }} className="flex justify-end items-center">
+                    <div onClick={() => setSorter('priceChangePercent')} style={{ flex: '1 1 0%' }} className="flex justify-end items-center select-none">
                     {t('futures:24h_change')}
                         <Sorter isUp={sortBy?.[`priceChangePercent`]} />
                     </div>
