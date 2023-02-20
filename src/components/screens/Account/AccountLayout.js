4bc59b94013077a6df0797b9cfd27d9db9397c16
ryan <phudq@nami.trade>
2023-02-20 16:45:40 +0700

    fix(account): fix QC bug list + add s3 url images

--- src/components/screens/Account/AccountLayout.js
+++ src/components/screens/Account/AccountLayout.js
@@ -14,20 +14,28 @@ import { KYC_STATUS } from 'redux/actions/const';
 import Link from 'next/link';
 import AccountAvatar from 'components/screens/Account/AccountAvatar';
 import { PATHS } from 'constants/paths';
-import { getS3Url } from 'redux/actions/utils';
+import { getLoginUrl, getS3Url } from 'redux/actions/utils';
 import useDarkMode, { THEME_MODE } from 'hooks/useDarkMode';
+import { useEffect } from 'react';
 
 const KYCPendingTag = ({ t }) => <TagV2 className='ml-3' type='warning'>{t('profile:kyc_wait')}</TagV2>;
 
 const KYCVerifiedTag = ({ t }) => <TagV2 className='ml-3' type='success'>{t('profile:kyc_verified')}</TagV2>;
 
 export default function AccountLayout({ children }) {
-    const user = useSelector((state) => state.auth?.user);
+    const auth = useSelector((state) => state.auth);
 
     const router = useRouter();
     const isApp = useApp();
     const { t } = useTranslation();
     const [currentTheme] = useDarkMode();
+
+    useEffect(() => {
+        // Not logged
+        if (!auth.user && !auth.loadingUser) {
+            window.open(getLoginUrl('sso', 'login'), '_self');
+        }
+    }, [auth])
 
     return <MaldivesLayout
         dark={currentTheme === THEME_MODE.DARK}
@@ -37,56 +45,64 @@ export default function AccountLayout({ children }) {
         <div
             className='bg-black-800 h-24 md:h-44'
             style={{ // TODO: image s3
-                backgroundImage: `url(/images/screen/account/banner_2.png)`,
+                backgroundImage: `url(${getS3Url('/images/screen/account/banner_2.png')})`,
                 backgroundSize: 'auto 100%',
                 backgroundPosition: 'center'
             }}
         />
         <Container className='mal-container px-4 h-full'>
             <div className='flex flex-col md:flex-row items-center md:items-end justify-center md:justify-between'>
-                <AccountAvatar currentAvatar={user?.avatar} />
-                <div className='mt-6 md:mt-0 md:ml-4 flex-1 flex flex-col md:flex-row items-center md:items-start'>
-                    <div className='mb-3 md:mb-0'>
-                        <div className='text-xl md:text-2xl leading-7 mb-2 font-semibold'>{user?.name}</div>
-                        <TextCopyable text={user?.code}
-                                      className='text-sm md:text-base text-txtSecondary dark:text-txtSecondary-dark justify-center md:justify-start' />
-                    </div>
+                <AccountAvatar currentAvatar={auth?.user?.avatar} />
+                <div className='mt-6 md:mt-0 md:ml-4 flex-1 flex flex-col md:flex-row justify-between items-center'>
+                    <div className='flex flex-col md:flex-row items-center md:items-start'>
+                        <div className='mb-3 md:mb-0'>
+                            <div className='text-xl md:text-2xl !leading-7 mb-2 font-semibold'>{auth?.user?.name}</div>
+                            <TextCopyable
+                                text={auth?.user?.code}
+                                className='text-sm md:text-base text-txtSecondary dark:text-txtSecondary-dark justify-center md:justify-start'
+                            />
+                        </div>
 
-                    {{
-                        [KYC_STATUS.PENDING_APPROVAL]: <KYCPendingTag t={t} />,
-                        [KYC_STATUS.APPROVED]: <KYCVerifiedTag t={t} />
-                    }[KYC_STATUS.PENDING_APPROVAL] || null}
+                        {{
+                            [KYC_STATUS.PENDING_APPROVAL]: <KYCPendingTag t={t} />,
+                            [KYC_STATUS.APPROVED]: <KYCVerifiedTag t={t} />
+                        }[auth?.user?.kyc_status] || null}
+                    </div>
+                    {
+                        auth?.user?.kyc_status === KYC_STATUS.NO_KYC && router.asPath === PATHS.ACCOUNT.PROFILE &&
+                        <Button
+                            className='w-[90%] md:w-auto px-6 mt-4 md:mt-0'
+                            onClick={() => {
+                                router.push(PATHS.ACCOUNT.IDENTIFICATION, null, { scroll: false });
+                            }}
+                        >{t('profile:verify_account')}</Button>
+                    }
                 </div>
-                {
-                    user?.kyc_status === KYC_STATUS.NO_KYC && router.asPath === PATHS.ACCOUNT.PROFILE &&
-                    <Button
-                        className='w-auto px-6'
-                        onClick={() => {
-                            router.push(PATHS.ACCOUNT.IDENTIFICATION, null, { scroll: false });
-                        }}
-                    >{t('profile:verify_account')}</Button>
-                }
             </div>
 
             <div className='flex justify-between items-center border-b border-divider dark:border-divider-dark mt-12'>
                 <div className='flex space-x-6'>
-                    {[{
-                        label: t('navbar:menu.user.profile'),
-                        link: PATHS.ACCOUNT.PROFILE
-                    }, {
-                        label: t('identification:title_tab'),
-                        link: PATHS.ACCOUNT.IDENTIFICATION
-                    }, {
-                        label: t('reward-center:title'),
-                        link: PATHS.ACCOUNT.REWARD_CENTER
-                    }].map((item, index) => {
+                    {[
+                        {
+                            label: t('navbar:menu.user.profile'),
+                            link: PATHS.ACCOUNT.PROFILE
+                        },
+                        {
+                            label: t('identification:title_tab'),
+                            link: PATHS.ACCOUNT.IDENTIFICATION
+                        }
+                        // {
+                        //     label: t('reward-center:title'),
+                        //     link: PATHS.ACCOUNT.REWARD_CENTER
+                        // }
+                    ].map((item, index) => {
                         const isActive = item.link === router.asPath;
                         return (
-                            <Link scroll={false} href={item.link} key={index}>
+                            <Link scroll={false} href={item.link} key={item.link}>
                                 <div
-                                    className={classnames('py-4 font-semibold border-b-2 mb-[-1px] cursor-pointer', {
+                                    className={classnames('py-4 border-b-2 mb-[-1px] cursor-pointer', {
                                         'border-transparent text-txtSecondary dark:text-txtSecondary-dark': !isActive,
-                                        'border-teal text-txtPrimary dark:text-teal': isActive
+                                        'border-teal text-txtPrimary font-semibold dark:text-teal': isActive
                                     })}
                                 >
                                     {item.label}
@@ -97,7 +113,7 @@ export default function AccountLayout({ children }) {
                 </div>
                 <span
                     onClick={() => router.push(PATHS.FEE_STRUCTURES.TRADING)}
-                    className='hidden md:inline text-teal cursor-pointer font-medium hover:underline'>
+                    className='hidden md:inline text-teal cursor-pointer font-semibold hover:underline'>
                     {t('fee-structure:see_fee_structures')}
                 </span>
             </div>
