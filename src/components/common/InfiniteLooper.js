26201f2a6e82cd97ad2cfebeba172a66fd3255b4
giangnt <giangnt@nami.trade>
2023-02-22 11:45:08 +0700

    style: adding separator circle on currentactivity section

--- src/components/common/InfiniteLooper.js
+++ src/components/common/InfiniteLooper.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import classNames from 'classnames';
 
 // loopSize is the number of duplicates children.
 // loopDuration is the duration for one children to finish move on screen.
-const InfiniteLooper = ({ children, loopSize = 3, loopDuration = 30 }) => {
+const InfiniteLooper = ({ children, loopSize = 3, loopDuration = 30, onHoverPaused = true }) => {
     return (
-        <div className="looper">
-            {[...Array(loopSize).keys()].map((listNumber,index) => (
+        <div
+            className={classNames('looper', {
+                'looper-hover__paused': onHoverPaused
+            })}
+        >
+            {[...Array(loopSize).keys()].map((listNumber, index) => (
                 <div
                     className="looper__listInstance"
                     style={{ animationDuration: `${loopDuration}s` }}
