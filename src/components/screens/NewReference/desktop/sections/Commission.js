f8a4696ef64830e9f308bc3c5b2af89d4699cf85
lamnv@nami.trade <lamnv@nami.trade>
2023-02-25 15:02:19 +0700

    fix: ranking

--- src/components/screens/NewReference/desktop/sections/Commission.js
+++ src/components/screens/NewReference/desktop/sections/Commission.js
@@ -3,7 +3,7 @@ import FetchApi from 'utils/fetch-api';
 import { API_NEW_REFERRAL_NEW_COMMISSIONS, API_NEW_REFERRAL_NEW_FRIENDS } from 'redux/actions/apis';
 import { Line, NoData } from 'components/screens/NewReference/mobile';
 import { formatNumber, formatTime } from 'redux/actions/utils';
-import React, { useEffect, useState, useMemo } from 'react'
+import React, { useEffect, useState, useMemo } from 'react';
 import RefCard from '../../RefCard';
 import useDarkMode, { THEME_MODE } from 'hooks/useDarkMode';
 import { pick } from 'lodash';
@@ -31,8 +31,8 @@ const tier = {
     5: {
         en: 'Diamond',
         vi: 'Kim Cương'
-    },
-}
+    }
+};
 const languages = {
     newUser: {
         en: 'New Friend',
@@ -50,113 +50,115 @@ const languages = {
         en: 'Ranking',
         vi: 'Hạng'
     }
-}
+};
 
 const Commission = ({ t, language, id }) => {
-    const [lastedCommissions, setLastedCommissions] = useState([])
-    const [lastedFriends, setLastedFriends] = useState([])
-    const [theme] = useDarkMode()
+    const [lastedCommissions, setLastedCommissions] = useState([]);
+    const [lastedFriends, setLastedFriends] = useState([]);
+    const [theme] = useDarkMode();
     useEffect(() => {
         FetchApi({
             url: API_NEW_REFERRAL_NEW_COMMISSIONS,
             options: {
-                method: 'GET',
-            },
+                method: 'GET'
+            }
         }).then(({ data, status }) => {
             if (status === 'ok') {
-                setLastedCommissions(data)
+                setLastedCommissions(data);
             } else {
-                setLastedCommissions([])
+                setLastedCommissions([]);
             }
         });
         FetchApi({
             url: API_NEW_REFERRAL_NEW_FRIENDS,
             options: {
-                method: 'GET',
-            },
+                method: 'GET'
+            }
         }).then(({ data, status }) => {
             if (status === 'ok') {
-                setLastedFriends(data)
+                setLastedFriends(data);
             } else {
-                setLastedFriends([])
+                setLastedFriends([]);
             }
         });
-    }, [])
+    }, []);
 
     const renderNewFriends = useMemo(() => {
-        return !lastedFriends.length ? <><NoData className='my-20' text={t('reference:referral.no_friends')} /></> : lastedFriends.map(data =>
-            <div key={data.userId} className='p-4 bg-gray-13 dark:bg-dark-2 rounded-xl'>
-                <div className='flex gap-2 mb-6'>
-                    <UserCircle/>
-                    <p className='font-semibold'>
-                        {t('reference:referral.new_friend')}: {data.code}
-                    </p>
+        return !lastedFriends.length ? (
+            <>
+                <NoData className="my-20" text={t('reference:referral.no_friends')} />
+            </>
+        ) : (
+            lastedFriends.map((data) => (
+                <div key={data.userId} className="p-4 bg-gray-13 dark:bg-dark-2 rounded-xl">
+                    <div className="flex gap-2 mb-6">
+                        <UserCircle />
+                        <p className="font-semibold">
+                            {t('reference:referral.new_friend')}: {data.code}
+                        </p>
+                    </div>
+                    <div className="space-y-3">
+                        <div>
+                            <span className="text-txtSecondary dark:text-txtSecondary-dark">
+                                <span>Nami ID </span>
+                                <span className="text-xs">({t('reference:referral:referrer')})</span>
+                            </span>
+                            <span className="float-right text-teal font-semibold">{data.code}</span>
+                        </div>
+                        <div>
+                            <span className="text-txtSecondary dark:text-txtSecondary-dark">{t('reference:referral:level')}</span>
+                            <span className="float-right">{data.level}</span>
+                        </div>
+                        <div>
+                            <span className="text-txtSecondary dark:text-txtSecondary-dark">{t('reference:referral:ranking')}</span>
+                            <span className="float-right">{tier[data.rank][language]}</span>
+                        </div>
+                    </div>
                 </div>
-                <div className='space-y-3'>
-                    <div>
-                        <span className='text-txtSecondary dark:text-txtSecondary-dark'>
-                            <span>Nami ID </span><span className='text-xs'>({t('reference:referral:referrer')})</span>
-                        </span>
-                        <span className='float-right text-teal font-semibold'>{data.code}</span>
-                    </div>
-                    <div>
-                        <span className='text-txtSecondary dark:text-txtSecondary-dark'>{t('reference:referral:level')}</span>
-                        <span className='float-right'>{data.level}</span>
-                    </div>
-                    <div>
-                        <span className='text-txtSecondary dark:text-txtSecondary-dark'>{t('reference:referral:rank')}</span>
-                        <span className='float-right'>{tier[data.rank][language]}</span>
-                    </div>
-                </div>
-            </div>
-        )
-    }, [lastedFriends])
+            ))
+        );
+    }, [lastedFriends]);
 
     const renderLastedCommissions = useMemo(() => {
-        return !lastedCommissions.length ? <><NoData className='my-20' text={t('reference:referral.no_commission')} /></> : lastedCommissions.map((data, index) =>
-            <div key={index}>
-                <div className='flex flex-col gap-1'>
-                    <div className='flex w-full justify-between items-center font-semibold leading-6'>
-                        <div>
-                            {data.formUserCode} ({t('reference:referral.level')} {data.level < 10 ? 0 : null}{data.level})
+        return !lastedCommissions.length ? (
+            <>
+                <NoData className="my-20" text={t('reference:referral.no_commission')} />
+            </>
+        ) : (
+            lastedCommissions.map((data, index) => (
+                <div key={index}>
+                    <div className="flex flex-col gap-1">
+                        <div className="flex w-full justify-between items-center font-semibold leading-6">
+                            <div>
+                                {data.formUserCode} ({t('reference:referral.level')} {data.level < 10 ? 0 : null}
+                                {data.level})
+                            </div>
+                            <div className="text-teal">+{formatNumber(data.value, 2)} VNDC</div>
                         </div>
-                        <div className='text-teal'>
-                            +{formatNumber(data.value, 2)} VNDC
-                        </div>
-                    </div>
-                    <div className='flex w-full justify-between items-center text-txtSecondary dark:text-txtSecondary-dark text-sm'>
-                        <div>
-                            {formatTime(data.createdAt, 'yyyy-MM-dd HH:mm:ss')}
-                        </div>
-                        <div>
-                            {t('reference:referral.type')}: <span className='capitalize'>{data.kind?.toLowerCase()}</span>
+                        <div className="flex w-full justify-between items-center text-txtSecondary dark:text-txtSecondary-dark text-sm">
+                            <div>{formatTime(data.createdAt, 'yyyy-MM-dd HH:mm:ss')}</div>
+                            <div>
+                                {t('reference:referral.type')}: <span className="capitalize">{data.kind?.toLowerCase()}</span>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        )
-    }, [lastedCommissions])
+            ))
+        );
+    }, [lastedCommissions]);
 
     return (
-        <div className='flex gap-8 w-full' id={id}>
-            <RefCard wrapperClassName='!py-6 px-8 w-full max-h-[624px]' isBlack={theme === THEME_MODE.DARK}>
-                <div className='font-semibold text-[22px] leading-7 py-3 mb-6'>
-                    {t('futures:mobile.commission')}
-                </div>
-                <div className='max-h-[calc(624px-124px)] overflow-y-auto pr-5 -mr-5 space-y-8'>
-                    {renderLastedCommissions}
-                </div>
+        <div className="flex gap-8 w-full" id={id}>
+            <RefCard wrapperClassName="!py-6 px-8 w-full max-h-[624px]" isBlack={theme === THEME_MODE.DARK}>
+                <div className="font-semibold text-[22px] leading-7 py-3 mb-6">{t('futures:mobile.commission')}</div>
+                <div className="max-h-[calc(624px-124px)] overflow-y-auto pr-5 -mr-5 space-y-8">{renderLastedCommissions}</div>
             </RefCard>
-            <RefCard wrapperClassName='!py-6 px-8 w-full max-h-[624px]' isBlack={theme === THEME_MODE.DARK}>
-                <div className='font-semibold text-[22px] leading-7 py-3 mb-6'>
-                    {t('reference:referral.new_friends')}
-                </div>
-                <div className='max-h-[calc(624px-124px)] overflow-y-auto pr-5 -mr-5 space-y-8'>
-                    {renderNewFriends}
-                </div>
+            <RefCard wrapperClassName="!py-6 px-8 w-full max-h-[624px]" isBlack={theme === THEME_MODE.DARK}>
+                <div className="font-semibold text-[22px] leading-7 py-3 mb-6">{t('reference:referral.new_friends')}</div>
+                <div className="max-h-[calc(624px-124px)] overflow-y-auto pr-5 -mr-5 space-y-8">{renderNewFriends}</div>
             </RefCard>
         </div>
-    )
-}
+    );
+};
 
-export default Commission
+export default Commission;
