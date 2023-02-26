7808c1e7520828286d11df3eca8f0cf8b8f9a6a4
giangnt@nami.trade <giangnt@nami.trade>
2023-02-26 15:33:08 +0700

    style: adding new behavior for language select on nav

--- src/components/common/NavBar/LanguageSetting.js
+++ src/components/common/NavBar/LanguageSetting.js
@@ -1,53 +1,32 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import SvgGlobe from 'components/svg/Globe';
 import { CheckCircleIcon } from 'components/svg/SvgIcon';
 import useLanguage, { LANGUAGE_TAG } from 'hooks/useLanguage';
-import { useClickAway, useHoverDirty, useToggle } from 'react-use';
-import colors from 'styles/colors';
 
 const LanguageSetting = () => {
-    const [isOpen, toggleOpen] = useToggle(false);
-
-    const ref = useRef(null);
-
-    // const isHovering = useHoverDirty(ref);
     const [currentLocale, onChangeLang] = useLanguage();
 
-    useClickAway(ref, () => {
-        if (isOpen) {
-            toggleOpen();
-        }
-    });
-
     return (
-        <div
-            //  ref={ref}
-            className=" flex items-center h-full group mal-navbar__hamburger__spacing "
-            //  onClick={toggleOpen}
-        >
+        <div className=" flex items-center group mal-navbar__hamburger__spacing relative">
             <div className="mal-navbar__svg_dominant cursor-pointer text-txtSecondary dark:text-txtSecondary-dark">
                 <SvgGlobe type={2} size={20} color="currentColor" />
             </div>
 
-            {/* <div onClick={onChangeLang} className="cursor-pointer text-dominant uppercase font-semibold hover:opacity-80 transition-opacity">
-                {currentLocale}
-            </div> */}
-
-            {/* {isOpen && ( */}
-            <div className="absolute hidden group-hover:block mt-[1px] bg-white dark:bg-bgTabInactive-dark py-2 border border-t-0 dark:border-divider-dark !min-w-[216px] top-full right-4 rounded-b-xl  ">
-                {Object.values(LANGUAGE_TAG).map((lang) => (
-                    <button
-                        key={lang}
-                        disabled={lang === currentLocale}
-                        onClick={() => onChangeLang(lang)}
-                        className="py-3 w-full cursor-pointer items-center text-textPrimary px-4 flex justify-between dark:hover:bg-hover-dark hover:bg-hover first:mb-3 disabled:cursor-default"
-                    >
-                        {(lang === LANGUAGE_TAG.EN && 'English') || (lang === LANGUAGE_TAG.VI && 'Tiếng Việt')}
-                        {currentLocale === lang && <CheckCircleIcon color="currentColor" size={16} />}
-                    </button>
-                ))}
+            <div className="absolute hidden group-hover:block top-full pt-[29px] right-0 ">
+                <div className=" mt-[1px] bg-white dark:bg-bgTabInactive-dark py-2 border border-t-0 dark:border-divider-dark !min-w-[216px] rounded-b-xl  ">
+                    {Object.values(LANGUAGE_TAG).map((lang) => (
+                        <button
+                            key={lang}
+                            disabled={lang === currentLocale}
+                            onClick={() => onChangeLang(lang)}
+                            className="py-3 w-full cursor-pointer items-center text-textPrimary px-4 flex justify-between dark:hover:bg-hover-dark hover:bg-hover first:mb-3 disabled:cursor-default"
+                        >
+                            {(lang === LANGUAGE_TAG.EN && 'English') || (lang === LANGUAGE_TAG.VI && 'Tiếng Việt')}
+                            {currentLocale === lang && <CheckCircleIcon color="currentColor" size={16} />}
+                        </button>
+                    ))}
+                </div>
             </div>
-            {/* )} */}
         </div>
     );
 };
