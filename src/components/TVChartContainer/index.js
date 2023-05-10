69493c6bb1a5c0f7195f96b0bdf21fb6cd50b9d9
nhatlong1303 <nguyennhatlong1303@gmail.com>
2023-05-10 15:17:58 +0700

    Setting - add setting futures

--- src/components/TVChartContainer/index.js
+++ src/components/TVChartContainer/index.js
@@ -12,6 +12,7 @@ import { VndcFutureOrderType } from '../screens/Futures/PlaceOrder/Vndc/VndcFutu
 import { isMobile } from 'react-device-detect';
 import { debounce, set } from 'lodash';
 import Spiner from 'components/common/V2/LoaderV2/Spiner';
+import { FuturesSettings } from 'redux/reducers/futures';
 
 const CONTAINER_ID = 'nami-tv';
 const CHART_VERSION = '1.0.9';
@@ -76,8 +77,22 @@ export class TVChartContainer extends React.PureComponent {
                 this.drawHighLowArrows();
             }
         }
-        if (prevProps.ordersList !== this.props.ordersList && !this.firstTime) {
+        if (prevProps.ordersList !== this.props.ordersList && !this.firstTime && this.props?.isShowSlTPLine) {
             this.rawOrders();
+        }
+
+        if (prevProps?.isShowSlTPLine !== this.props?.isShowSlTPLine && !this.firstTime) {
+            if (this.props?.isShowSlTPLine) {
+                this.rawOrders();
+            } else {
+                this.drawnSl = null;
+                this.drawnTp = null;
+                this.oldOrdersList = [];
+                Object.keys(this.drawnOrder).map((line) => {
+                    this.drawnOrder[line].remove();
+                    delete this.drawnOrder[line];
+                });
+            }
         }
     }
 
@@ -478,7 +493,7 @@ export class TVChartContainer extends React.PureComponent {
             this.setState({ chartStatus: ChartStatus.LOADED });
             if (this.timer) clearTimeout(this.timer);
             this.timer = setTimeout(() => {
-                this.rawOrders();
+                if (this.props?.isShowSlTPLine) this.rawOrders();
                 this.drawHighLowArrows();
                 this.firstTime = false;
                 this.widget
--- src/components/chart/FuturesChart.js
+++ src/components/chart/FuturesChart.js
@@ -3,13 +3,16 @@ import useDarkMode from 'hooks/useDarkMode';
 import { ChartMode } from 'redux/actions/const';
 import { useSelector } from 'react-redux';
 import { useState } from 'react';
-
+import { FuturesSettings } from 'redux/reducers/futures';
 const FuturesChart = dynamic(() => import('src/components/TVChartContainer/').then((mod) => mod.TVChartContainer), { ssr: false });
 
 export default (props) => {
     const [currentTheme] = useDarkMode();
     const exchangeConfig = useSelector((state) => state.utils.exchangeConfig);
+    const settings = useSelector((state) => state.futures.settings);
     const [chartKey, setChartKey] = useState('nami-spot-chart');
+    const isShowSlTPLine = settings?.user_setting?.[FuturesSettings.show_sl_tp_order_line] ?? true;
+
     return (
         <FuturesChart
             {...props}
@@ -18,6 +21,7 @@ export default (props) => {
             theme={currentTheme}
             mode={ChartMode.FUTURES}
             exchangeConfig={exchangeConfig}
+            isShowSlTPLine={isShowSlTPLine}
         />
     );
 };
