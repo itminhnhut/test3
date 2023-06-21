2640019244bce6aa1e0705e870236acbfe3b5086
giangnt@nami.trade <giangnt@nami.trade>
2023-06-21 16:58:07 +0700

    fix: buglist

--- src/components/staking/FAQ.js
+++ src/components/staking/FAQ.js
@@ -11,17 +11,17 @@ const FAQStaking = ({ isDark }) => {
     } = useTranslation();
 
     const renderFAQ = useMemo(() => {
-        return FAQ.map((item, key) => {
+        return FAQ.map((item, index) => {
             return (
                 <CollapseV2
-                    key={`FAQ-${key}`}
-                    className="w-full divider-bottom lg:last:pb-[120px] last:pb-[80px]"
+                    key={`FAQ-${index}`}
+                    className="w-full lg:last:pb-[120px] last:pb-[80px]"
                     divLabelClassname="w-full justify-between !pb-5 !mb-0"
                     chrevronStyled={{ size: 24, color: isDark ? colors.gray['4'] : colors.gray['15'] }}
                     label={item.title?.[language]}
                     labelClassname="text-base lg:text-[18px] font-semibold mt-5 text-gray-15 dark:text-white w-10/12"
                     chevronDownClassName="mt-5"
-                    isDividerBottom={true}
+                    isDividerBottom={index !== FAQ.length - 1}
                     dividerBottomClassName="!mt-5"
                 >
                     <ul className="list-disc pl-5">
@@ -33,7 +33,7 @@ const FAQStaking = ({ isDark }) => {
     }, [isDark]);
 
     return (
-        <section className="max-w-screen-v3 2xl:max-w-screen-xxl m-auto px-4 mt-[88px]">
+        <section className="max-w-screen-v3  2xl:max-w-screen-xxl m-auto px-4 mt-[88px]">
             <h2 className="text-2xl lg:text-5xl font-semibold text-gray-15 dark:text-gray-4">Các câu hỏi thường gặp (FAQ)</h2>
             <div className="mt-8 lg:mt-[60px]">{renderFAQ}</div>
         </section>
