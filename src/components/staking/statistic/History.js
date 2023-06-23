8c5f388c9aebbb3ba4f958695f859d8b79f2e69e
nhutnhm <nhutnhm@edoctor.vn>
2023-06-23 19:41:27 +0700

    add api history

--- src/components/staking/statistic/History.js
+++ src/components/staking/statistic/History.js
@@ -1,16 +1,24 @@
 import { useMemo, useState, useCallback, useEffect, useRef } from 'react';
 
 import { useTranslation } from 'next-i18next';
-import TableV2 from 'components/common/V2/TableV2';
-import Chip from 'components/common/V2/Chip';
-import DatePickerV2 from 'components/common/DatePicker/DatePickerV2';
-import FetchApi from 'utils/fetch-api';
-import { formatNumber, formatTime } from 'redux/actions/utils';
-import { API_HISTORY_STAKING_DAILY } from 'redux/actions/apis';
-import { useSelector } from 'react-redux';
-import { endOfDay, startOfDay, subDays, getTime } from 'date-fns';
 
+import Chip from 'components/common/V2/Chip';
+import TableV2 from 'components/common/V2/TableV2';
+import AssetLogo from 'components/wallet/AssetLogo';
+import DatePickerV2 from 'components/common/DatePicker/DatePickerV2';
+
+import FetchApi from 'utils/fetch-api';
+import { API_HISTORY_STAKING_DAILY, API_GET_COMMISSION_HISTORY_PARTNER } from 'redux/actions/apis';
+
+import { formatNumber, formatTime } from 'redux/actions/utils';
+import { useSelector } from 'react-redux';
+
+import { useWindowSize } from 'react-use';
+import { endOfDay, startOfDay, subDays, getTime, isValid } from 'date-fns';
+
+import styled from 'styled-components';
 import classNames from 'classnames';
+
 const newDate = new Date();
 
 const DATE_OPTIONS = [
@@ -45,10 +53,9 @@ const initState = {
     loading: false,
     page: 1,
     dataSource: {
-        results: [],
+        result: [],
         totalProfit: { value: 0 },
         hasNext: false,
-        total: 0,
         go_next: true
     },
     filter: {
@@ -66,6 +73,9 @@ const HistoryStaking = ({ assetId }) => {
         i18n: { language }
     } = useTranslation();
 
+    const { width } = useWindowSize();
+    const isMobile = width < 830;
+
     const assetConfigs = useSelector((state) => state.utils?.assetConfig) || [];
     const asset = useMemo(() => {
         return assetConfigs.find((asset) => asset.id === assetId);
@@ -75,6 +85,8 @@ const HistoryStaking = ({ assetId }) => {
     const [page, setPage] = useState(initState.page);
     const [range, setRange] = useState(initState.range);
     const [dataSource, setDataSource] = useState(initState.dataSource);
+    const [dataSource2, setDataSource2] = useState(initState.dataSource);
+
     const [filter, setFilter] = useState(initState.filter);
 
     const refAsset = useRef(initState.defaultAsset);
@@ -84,8 +96,16 @@ const HistoryStaking = ({ assetId }) => {
         if (refAsset.current !== assetId) {
             handleResetByAssetId();
         }
+        handleOverviewAPI();
+    }, [assetId, range, filter?.range?.startDate, filter?.range?.endDate]);
+
+    useEffect(() => {
+        // reset data change assetId (VNDC, USDT)
+        if (refAsset.current !== assetId) {
+            handleResetByAssetId();
+        }
         handleHistoryAPI();
-    }, [assetId, range]);
+    }, [assetId, range, page, filter?.range?.startDate, filter?.range?.endDate]);
 
     // handle reset data
     const handleResetByAssetId = () => {
@@ -95,9 +115,8 @@ const HistoryStaking = ({ assetId }) => {
         refAsset.current = assetId;
     };
 
-    const handleHistoryAPI = async () => {
+    const handleOverviewAPI = async () => {
         try {
-            setLoading(true);
             const { data, message } = await FetchApi({
                 url: API_HISTORY_STAKING_DAILY,
                 options: {
@@ -111,6 +130,33 @@ const HistoryStaking = ({ assetId }) => {
             });
             if (data) {
                 setDataSource({ ...data });
+            } else {
+                console.error(message, { cause: 'Error API Staking History' });
+            }
+        } catch (error) {
+            console.error(error, { cause: 'Error API Staking History' });
+        }
+    };
+
+    const handleHistoryAPI = async () => {
+        try {
+            setLoading(true);
+            const { data, message } = await FetchApi({
+                url: API_GET_COMMISSION_HISTORY_PARTNER,
+                options: {
+                    method: 'GET'
+                },
+                params: {
+                    type: 'staking',
+                    currency: assetId,
+                    limit: LIMIT,
+                    skip: (page - 1) * LIMIT,
+                    ...(filter.range.startDate && { from: getTime(new Date(filter.range.startDate)) }),
+                    ...(filter.range.endDate && { to: getTime(new Date(filter.range.endDate)) })
+                }
+            });
+            if (data) {
+                setDataSource2({ ...data });
             } else {
                 console.error(message, { cause: 'Error API Staking History' });
             }
@@ -160,38 +206,45 @@ const HistoryStaking = ({ assetId }) => {
     const renderTable = useCallback(() => {
         const columns = [
             {
-                key: 'name',
-                dataIndex: 'name',
+                key: 'currency',
+                dataIndex: 'currency',
                 title: t('staking:statics:history:columns.type'),
                 align: 'left',
-                render: (value) => value || '_'
+                render: (value) => (
+                    <section className="flex flex-row items-center">
+                        <AssetLogo assetId={value} />
+                        <span className="ml-2">{asset?.assetCode}</span>
+                    </section>
+                )
             },
             {
-                key: 'code',
-                dataIndex: 'code',
+                key: 'money_use',
+                dataIndex: 'money_use',
                 title: t('staking:statics:history:columns.amount'),
                 align: 'left',
-                render: (value) => value || '_'
+                render: (value) => <div>{formatNumber(value, asset?.assetDigit)}</div>
             },
             {
-                key: 'code',
-                dataIndex: 'code',
+                key: 'created_at',
+                dataIndex: 'created_at',
                 title: t('staking:statics:history:columns.time'),
                 align: 'left',
-                render: (value) => value || '_'
+                render: (value) => (
+                    <div className="font-normal">{value && isValid(new Date(value)) ? formatTime(new Date(value), 'HH:mm:ss dd/MM/yyyy') : null}</div>
+                )
             },
             {
                 key: 'code',
                 dataIndex: 'code',
                 title: t('staking:statics:history:columns.status'),
                 align: 'left',
-                // width: 220,
-                render: (value) => value || '_'
+                render: (value) => <div className="dark:text-green-2 text-green-3">{t('common:success')}</div>
             }
         ];
 
         return (
-            <TableV2
+            <WrapperTable
+                isMobile={isMobile}
                 skip={0}
                 useRowHover
                 height={350}
@@ -200,11 +253,11 @@ const HistoryStaking = ({ assetId }) => {
                 columns={columns}
                 scroll={{ x: true }}
                 className="border-t border-divider dark:border-divider-dark"
-                data={dataSource?.results || []}
+                data={dataSource2?.result || []}
                 rowKey={(item) => `${item?.key}`}
                 pagingPrevNext={{
                     page: page - 1,
-                    hasNext: dataSource?.hasNext,
+                    hasNext: dataSource2?.hasNext,
                     onChangeNextPrev: (delta) => {
                         setPage(page + delta);
                     },
@@ -212,24 +265,24 @@ const HistoryStaking = ({ assetId }) => {
                 }}
             />
         );
-    }, [dataSource, loading]);
+    }, [dataSource2?.result, loading]);
 
     return (
         <section>
             <h2 className="font-semibold text-2xl mt-[60px]">{t('staking:statics:history.title')}</h2>
             <section className="rounded-xl border-[1px] border-[#30BF73] dark:border-[#222940] mt-8">
-                <section className="flex flex-row my-8 ml-6 mr-[21px] h-[62px] items-center">
-                    <section className="w-2/5">
+                <section className="flex flex-col lg:flex-row my-8 ml-6 mr-[21px] h-full lg:h-[62px] items-center">
+                    <section className="w-full lg:w-2/5">
                         <div className="text-gray-15 dark:text-gray-7">{t('staking:statics:history.profit_received')}</div>
                         <div className="text-txtPrimary dark:text-gray-4 font-semibold mt-2">{totalProfit}</div>
                     </section>
-                    <section className="w-3/5 flex flex-row items-center gap-x-2 justify-end">
+                    <section className="w-full mt-2 lg:mt-0 lg:w-3/5 flex  lg:flex-nowrap flex-wrap flex-row items-center gap-2 lg:gap-x-2 justify-start lg:justify-end">
                         {renderDateOptions}
                         <DatePickerV2
-                            month={2}
                             hasShadow
                             position="right"
                             initDate={filter.range}
+                            month={isMobile ? 1 : 2}
                             onChange={handleChangeDate}
                             text={
                                 <Chip
@@ -249,4 +302,14 @@ const HistoryStaking = ({ assetId }) => {
         </section>
     );
 };
+
+const WrapperTable = styled(TableV2)`
+    .rc-table-container {
+        overflow: auto;
+        .rc-table-content {
+            width: ${(props) => (props.isMobile ? 'max-content' : '100%')};
+        }
+    }
+`;
+
 export default HistoryStaking;
