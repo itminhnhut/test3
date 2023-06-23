9764ac8c19976ed14bdbf9d7e3dd033741a8780b
giangnt@nami.trade <giangnt@nami.trade>
2023-06-23 14:58:03 +0700

    feat: adding interest estimate card

--- src/components/staking/APYInterestChart.js
+++ src/components/staking/APYInterestChart.js
@@ -30,16 +30,17 @@ const series = [
     }
 ];
 
-const getApyByMonth = ({ allowAmount, amount, percentPerDay, numberOfMonth }) => {
-    const monthsToDays = {
-        1: 30,
-        12: 365,
-        24: 730,
-        36: 1095,
-        48: 1460
-    };
-    // công thức tính lãi kép theo tháng
-    const compoundInterestAmount = +allowAmount * Math.pow(1 + percentPerDay / 100, monthsToDays[+numberOfMonth] || numberOfMonth * 30);
+const MONTHS_TO_DAYS = {
+    1: 30,
+    12: 365,
+    24: 730,
+    36: 1095,
+    48: 1460
+};
+
+export const getApyByDay = ({ allowAmount, amount, percentPerDay, numberOfDays }) => {
+    // công thức tính lãi kép theo từng ngày
+    const compoundInterestAmount = +allowAmount * Math.pow(1 + percentPerDay / 100, numberOfDays);
     const interestAmount = +compoundInterestAmount - +allowAmount;
     return {
         interestAmount,
@@ -181,7 +182,8 @@ const APYInterestChart = ({ amount, currencyId, currencyDayInterest }) => {
 
     const apyByMonth = useMemo(() => {
         const allowAmount = amount > STAKING_RANGE[currencyId].max ? STAKING_RANGE[currencyId].max : amount;
-        return getApyByMonth({ allowAmount, amount, percentPerDay: currencyDayInterest, numberOfMonth: hoverData.index + 1 });
+        const month = hoverData.index + 1;
+        return getApyByDay({ allowAmount, amount, percentPerDay: currencyDayInterest, numberOfDays: MONTHS_TO_DAYS[month] || month * 30 });
     }, [hoverData.index, currencyDayInterest, currencyId, amount]);
 
     const isAmountSmallerThanMin = amount < STAKING_RANGE[currencyId].min;
--- src/components/staking/statistic/InterestEstimate.js
+++ src/components/staking/statistic/InterestEstimate.js
@@ -1,7 +1,7 @@
 import React, { useCallback, useMemo, useState } from 'react';
 import { HideIcon, SeeIcon } from 'components/svg/SvgIcon';
 import Image from 'next/image';
-import { formatNumber, getLoginUrl, getS3Url } from 'redux/actions/utils';
+import { formatNumber, getDayInterestPercent, getLoginUrl, getS3Url } from 'redux/actions/utils';
 import { useSelector } from 'react-redux';
 import ButtonV2 from 'components/common/V2/ButtonV2/Button';
 import Link from 'next/link';
@@ -9,19 +9,20 @@ import { useRouter } from 'next/router';
 import { PARTNER_WD_TABS, PATHS } from 'constants/paths';
 import { SECRET_STRING } from 'utils';
 import styled from 'styled-components';
+import { APY_PERCENT, STAKING_RANGE } from 'constants/staking';
+import { getApyByDay } from '../APYInterestChart';
 
 const InterestEstimate = ({ assetId }) => {
+    const router = useRouter();
     const [isHideBalance, setIsHideBalance] = useState(false);
     const assetBalance = useSelector((state) => state.wallet?.SPOT) || null;
-    const auth = useSelector((state) => state.auth?.user);
-
-    const router = useRouter();
-
     const assetConfigs = useSelector((state) => state.utils?.assetConfig) || [];
-
     const asset = useMemo(() => {
         return assetConfigs.find((asset) => asset.id === assetId);
     }, [assetConfigs, assetId]);
+    const auth = useSelector((state) => state.auth?.user);
+
+    const userTotalBalance = assetBalance?.[asset?.id]?.value - assetBalance?.[asset?.id]?.locked_value;
 
     const renderAvailableBalance = useCallback(
         () => (
@@ -32,22 +33,21 @@ const InterestEstimate = ({ assetId }) => {
                         {isHideBalance ? <HideIcon size={24} /> : <SeeIcon size={24} />}
                     </div>
                 </div>
-                <div className="flex items-end justify-between">
-                    <div className="flex items-center space-x-6">
-                        <div className="p-[14px] flex items-center justify-center rounded-full bg-gray-12 dark:bg-dark-2">
+                <div className="flex flex-wrap items-end justify-between">
+                    <div className="flex md:mb-0 mb-6 items-center space-x-6">
+                        <div className="p-[14px] flex-1 flex items-center justify-center rounded-full bg-gray-12 dark:bg-dark-2">
                             <Image src={getS3Url('/images/staking/ic_staking.png')} width={36} height={36} />
                         </div>
                         <div className="space-y-1 ">
                             <div className="text-4xl font-semibold">
-                                {isHideBalance ? SECRET_STRING : formatNumber(assetBalance?.[asset?.id]?.value - assetBalance?.[asset?.id]?.locked_value)}{' '}
-                                {asset?.assetCode}
+                                {isHideBalance ? SECRET_STRING : formatNumber(userTotalBalance, asset?.assetDigit)} {asset?.assetCode}
                             </div>
                             <div>
-                                Lãi suất: <span className="text-teal font-semibold">12.79%</span>
+                                Lãi suất: <span className="text-teal font-semibold">{APY_PERCENT[asset?.assetCode]}%</span>
                             </div>
                         </div>
                     </div>
-                    <div>
+                    <div className="w-full md:w-auto">
                         {!auth ? (
                             <a
                                 href={getLoginUrl('sso', 'login', {
@@ -56,11 +56,11 @@ const InterestEstimate = ({ assetId }) => {
                                     }?side=BUY&assetId=${assetId}`
                                 })}
                             >
-                                <ButtonV2 className="w-[120px]">Nạp</ButtonV2>
+                                <ButtonV2 className="md:w-[120px]">Nạp</ButtonV2>
                             </a>
                         ) : (
                             <Link className="w-full" href={`${PATHS.WITHDRAW_DEPOSIT.PARTNER}?side=BUY&assetId=${assetId}`}>
-                                <ButtonV2 className="w-[120px]">Nạp</ButtonV2>
+                                <ButtonV2 className="md:w-[120px]">Nạp</ButtonV2>
                             </Link>
                         )}
                     </div>
@@ -70,22 +70,54 @@ const InterestEstimate = ({ assetId }) => {
         [auth, isHideBalance, asset, assetBalance]
     );
 
-    const renderDayInterestCard = useCallback(() => {
-        return (
-            <Card>
-                <div className="mb-4 text-txtSecondary dark:text-txtSecondary-dark">
-                    Lãi suất: <span className="text-teal font-semibold">12.79%</span>
-                </div>
-                <div className="text-2xl">
-
-                </div>
-            </Card>
-        );
-    }, []);
+    const renderInterestCard = useCallback(
+        ({ numberOfDays, title, type = 'dayInterestPercent' }) => {
+            const allowAmount = userTotalBalance > STAKING_RANGE[asset?.id]?.max ? STAKING_RANGE[asset?.id]?.max : userTotalBalance;
+            const dayInterestPercent = getDayInterestPercent(APY_PERCENT[asset?.assetCode]);
+            return (
+                <Card>
+                    <div className="mb-4 text-txtSecondary dark:text-txtSecondary-dark">
+                        {title}{' '}
+                        <span className="text-teal font-semibold">
+                            {type === 'dayInterestPercent' ? dayInterestPercent : type === 'yearInterestPercent' ? APY_PERCENT[asset?.assetCode] : ''}%
+                        </span>
+                    </div>
+                    <div className="text-2xl font-semibold">
+                        {isHideBalance
+                            ? SECRET_STRING
+                            : formatNumber(
+                                  getApyByDay({
+                                      allowAmount,
+                                      amount: userTotalBalance,
+                                      numberOfDays: numberOfDays,
+                                      percentPerDay: dayInterestPercent
+                                  })?.interestAmount,
+                                  asset?.assetDigit
+                              )}{' '}
+                        {asset?.assetCode}
+                    </div>
+                </Card>
+            );
+        },
+        [asset, userTotalBalance, isHideBalance]
+    );
     return (
         <div className="flex flex-wrap -m-3">
             <div className="w-full p-3">{renderAvailableBalance()}</div>
-            <div className="w-1/2 p-3">{renderDayInterestCard()}</div>
+            <div className="w-full md:w-1/2 p-3">
+                {renderInterestCard({
+                    numberOfDays: 1,
+                    type: 'dayInterestPercent',
+                    title: 'Lợi nhuận ngày:'
+                })}
+            </div>
+            <div className="w-full md:w-1/2 p-3">
+                {renderInterestCard({
+                    numberOfDays: 365,
+                    type: 'yearInterestPercent',
+                    title: 'Lợi nhuận dự kiến hàng năm:'
+                })}
+            </div>
         </div>
     );
 };
@@ -93,5 +125,5 @@ const InterestEstimate = ({ assetId }) => {
 export default InterestEstimate;
 
 export const Card = styled.div.attrs({
-    className: 'rounded-xl p-8 bg-gray-12 dark:bg-bgContainer-dark'
+    className: 'rounded-xl p-6 md:p-8 bg-gray-12 dark:bg-bgContainer-dark'
 })``;
