7f03016354e4931e4863bd0dc9bc5d43fd171821
giangnt@nami.trade <giangnt@nami.trade>
2023-06-23 15:39:49 +0700

    feat: adding content for interest card

--- src/components/staking/statistic/InterestEstimate.js
+++ src/components/staking/statistic/InterestEstimate.js
@@ -11,24 +11,36 @@ import { SECRET_STRING } from 'utils';
 import styled from 'styled-components';
 import { APY_PERCENT, STAKING_RANGE } from 'constants/staking';
 import { getApyByDay } from '../APYInterestChart';
+import { useTranslation } from 'next-i18next';
+import Tooltip from 'components/common/Tooltip';
 
-const InterestEstimate = ({ assetId }) => {
+const InterestEstimate = ({ assetId, t }) => {
     const router = useRouter();
     const [isHideBalance, setIsHideBalance] = useState(false);
-    const assetBalance = useSelector((state) => state.wallet?.SPOT) || null;
+    const spotBalance = useSelector((state) => state.wallet?.SPOT) || null;
+    const futuresBalance = useSelector((state) => state.wallet?.FUTURES) || null;
     const assetConfigs = useSelector((state) => state.utils?.assetConfig) || [];
     const asset = useMemo(() => {
         return assetConfigs.find((asset) => asset.id === assetId);
     }, [assetConfigs, assetId]);
     const auth = useSelector((state) => state.auth?.user);
 
-    const userTotalBalance = assetBalance?.[asset?.id]?.value - assetBalance?.[asset?.id]?.locked_value;
+    const userTotalBalance =
+        spotBalance?.[asset?.id]?.value -
+        spotBalance?.[asset?.id]?.locked_value +
+        (futuresBalance?.[asset?.id]?.value - futuresBalance?.[asset?.id]?.locked_value);
 
     const renderAvailableBalance = useCallback(
         () => (
             <Card>
+                <Tooltip place="top" className={`max-w-[360px] !px-6 !py-3`} effect="solid" isV3 id="balance_description">
+                    <div className="max-w-[300px] text-sm z-50">{t('staking:statics.interest.balance_description')}</div>
+                </Tooltip>
                 <div className="text-txtSecondary dark:text-txtSecondary-dark flex items-center space-x-3 mb-[50px]">
-                    <div>Tài sản khả dụng</div>
+                    <div className="nami-underline-dotted" data-tip="" data-for="balance_description">
+                        {t('staking:statics.interest.balance')}
+                    </div>
+
                     <div className="w-6 h-6 flex items-center cursor-pointer" onClick={() => setIsHideBalance((prev) => !prev)}>
                         {isHideBalance ? <HideIcon size={24} /> : <SeeIcon size={24} />}
                     </div>
@@ -43,7 +55,7 @@ const InterestEstimate = ({ assetId }) => {
                                 {isHideBalance ? SECRET_STRING : formatNumber(userTotalBalance, asset?.assetDigit)} {asset?.assetCode}
                             </div>
                             <div>
-                                Lãi suất: <span className="text-teal font-semibold">{APY_PERCENT[asset?.assetCode]}%</span>
+                                {t('staking:statics.interest.apy_interest')} <span className="text-teal font-semibold">{APY_PERCENT[asset?.assetCode]}%</span>
                             </div>
                         </div>
                     </div>
@@ -56,18 +68,18 @@ const InterestEstimate = ({ assetId }) => {
                                     }?side=BUY&assetId=${assetId}`
                                 })}
                             >
-                                <ButtonV2 className="md:w-[120px]">Nạp</ButtonV2>
+                                <ButtonV2 className="md:w-[120px]">{t('staking:statics.interest.deposit_now')}</ButtonV2>
                             </a>
                         ) : (
                             <Link className="w-full" href={`${PATHS.WITHDRAW_DEPOSIT.PARTNER}?side=BUY&assetId=${assetId}`}>
-                                <ButtonV2 className="md:w-[120px]">Nạp</ButtonV2>
+                                <ButtonV2 className="md:w-[120px]">{t('staking:statics.interest.deposit_now')}</ButtonV2>
                             </Link>
                         )}
                     </div>
                 </div>
             </Card>
         ),
-        [auth, isHideBalance, asset, assetBalance]
+        [auth, isHideBalance, asset, userTotalBalance]
     );
 
     const renderInterestCard = useCallback(
@@ -108,14 +120,14 @@ const InterestEstimate = ({ assetId }) => {
                 {renderInterestCard({
                     numberOfDays: 1,
                     type: 'dayInterestPercent',
-                    title: 'Lợi nhuận ngày:'
+                    title: t('staking:statics.interest.daily_interest')
                 })}
             </div>
             <div className="w-full md:w-1/2 p-3">
                 {renderInterestCard({
                     numberOfDays: 365,
                     type: 'yearInterestPercent',
-                    title: 'Lợi nhuận dự kiến hàng năm:'
+                    title: t('staking:statics.interest.annual_interest')
                 })}
             </div>
         </div>
