cb906dd6ce6ed8f8fb2137e87212e959330aaf31
giangnt@nami.trade <giangnt@nami.trade>
2023-06-26 17:07:20 +0700

    fix: content

--- src/components/staking/statistic/NotAuth.js
+++ src/components/staking/statistic/NotAuth.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Card from './Card';
 import { getLoginUrl, getS3Url } from 'redux/actions/utils';
-import { useTranslation } from 'next-i18next';
+import { Trans, useTranslation } from 'next-i18next';
 
 const NotAuth = () => {
     const { t } = useTranslation();
@@ -11,21 +11,11 @@ const NotAuth = () => {
                 <div className="flex justify-center mb-3">
                     <img className="max-h-[124px]" src={getS3Url('/images/icon/ic_login.png')} />
                 </div>
-                <div className="flex space-x-1 text-txtSecondary dark:text-darkBlue-5 truncate overflow-x-auto">
-                    <a href={getLoginUrl('sso')}>
-                        <span
-                            className="text-green-3 dark:text-green-2 hover:underline cursor-pointer font-semibold"
-                            dangerouslySetInnerHTML={{ __html: t('common:sign_in') }}
-                        />
-                    </a>
-                    <div>{t('common:or')}</div>
-                    <a href={getLoginUrl('sso', 'register')}>
-                        <span
-                            className="text-green-3 dark:text-green-2 hover:underline cursor-pointer font-semibold"
-                            dangerouslySetInnerHTML={{ __html: t('common:sign_up') }}
-                        />
-                    </a>
-                    <div>{t('common:to_experience')}</div>
+                <div className=" text-txtSecondary dark:text-txtSecondary-dark text-center">
+                    <Trans i18nKey="staking:statics.not_auth">
+                        <a className="text-green-3 dark:text-green-2 hover:underline cursor-pointer font-semibold" href={getLoginUrl('sso', 'register')} />
+                        <a className="text-green-3 dark:text-green-2 hover:underline cursor-pointer font-semibold" href={getLoginUrl('sso')} />
+                    </Trans>
                 </div>
             </div>
         </Card>
