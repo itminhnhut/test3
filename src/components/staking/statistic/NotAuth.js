c744685cb0d3825babeeb1b4d343a3f82312aca7
giangnt@nami.trade <giangnt@nami.trade>
2023-06-26 16:57:39 +0700

    style: not auth component

--- src/components/staking/statistic/NotAuth.js
+++ src/components/staking/statistic/NotAuth.js
@@ -8,16 +8,22 @@ const NotAuth = () => {
     return (
         <Card className="flex items-center justify-center !py-[50px]">
             <div>
-                <div className="flex justify-center">
+                <div className="flex justify-center mb-3">
                     <img className="max-h-[124px]" src={getS3Url('/images/icon/ic_login.png')} />
                 </div>
                 <div className="flex space-x-1 text-txtSecondary dark:text-darkBlue-5 truncate overflow-x-auto">
                     <a href={getLoginUrl('sso')}>
-                        <span className="text-teal hover:underline cursor-pointer font-semibold" dangerouslySetInnerHTML={{ __html: t('common:sign_in') }} />
+                        <span
+                            className="text-green-3 dark:text-green-2 hover:underline cursor-pointer font-semibold"
+                            dangerouslySetInnerHTML={{ __html: t('common:sign_in') }}
+                        />
                     </a>
                     <div>{t('common:or')}</div>
                     <a href={getLoginUrl('sso', 'register')}>
-                        <span className="text-teal hover:underline cursor-pointer font-semibold" dangerouslySetInnerHTML={{ __html: t('common:sign_up') }} />
+                        <span
+                            className="text-green-3 dark:text-green-2 hover:underline cursor-pointer font-semibold"
+                            dangerouslySetInnerHTML={{ __html: t('common:sign_up') }}
+                        />
                     </a>
                     <div>{t('common:to_experience')}</div>
                 </div>
