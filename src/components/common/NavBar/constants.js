040b2e21e956f919998546191b1a7ba697962229
giangnt <gr3en4pple23@gmail.com>
2023-07-31 17:36:10 +0700

    fix: open newpage insurance tab

--- src/components/common/NavBar/constants.js
+++ src/components/common/NavBar/constants.js
@@ -97,6 +97,7 @@ export const NAV_DATA = [
                 localized: 'nami_insurance',
                 isNew: false,
                 notSameOrigin: true,
+                isTarget: true,
                 url: INSURANCE_URL + '/buy-covered',
                 child_lv2: []
             }
@@ -408,6 +409,7 @@ export const MOBILE_NAV_DATA = [
                 localized: 'nami_insurance',
                 isNew: false,
                 notSameOrigin: true,
+                isTarget: true,
                 url: INSURANCE_URL + '/buy-covered',
                 child_lv2: []
             }
