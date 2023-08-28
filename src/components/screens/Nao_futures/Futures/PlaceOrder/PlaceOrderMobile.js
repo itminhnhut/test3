2e44163032a5a116d0a41e2f7c812332b440ad77
nhatlong1303 <nguyennhatlong1303@gmail.com>
2023-08-28 11:24:27 +0700

    NAO - fix:feedback

--- src/components/screens/Nao_futures/Futures/PlaceOrder/PlaceOrderMobile.js
+++ src/components/screens/Nao_futures/Futures/PlaceOrder/PlaceOrderMobile.js
@@ -65,8 +65,9 @@ const PlaceOrder = ({ decimals, side, setSide, pair, isAuth, availableAsset, pai
         if (!pairConfig) return;
         // ? Subscribe publicSocket
         // ? Get Pair Ticker
-        Emitter.on(PublicSocketEvent.FUTURES_TICKER_UPDATE + pairConfig?.symbol, async (data) => {
-            if (pairConfig.symbol === data?.s && data?.p > 0) {
+        const _symbol = convertSymbol(pairConfig.symbol);
+        Emitter.on(PublicSocketEvent.FUTURES_TICKER_UPDATE + _symbol, async (data) => {
+            if (_symbol === data?.s && data?.p > 0) {
                 const _pairPrice = FuturesMarketWatch.create(data, pairConfig?.quoteAsset);
                 setPairPrice(_pairPrice);
             }
@@ -413,7 +414,7 @@ const PlaceOrder = ({ decimals, side, setSide, pair, isAuth, availableAsset, pai
             return false;
         }
         return true;
-    }, [isAuth, type, pairConfig, price, side, leverage, availableAsset, pairPrice]);
+    }, [isAuth, type, pairConfig, price, side, leverage, availableAsset, pairPrice, quoteQty]);
 
     const onChangeTpSL = () => {
         if (!isAuth) return;
@@ -427,6 +428,7 @@ const PlaceOrder = ({ decimals, side, setSide, pair, isAuth, availableAsset, pai
         }
         onBlurInput();
         const _price = getPrice(getType(type), side, price, pairPrice?.ask, pairPrice?.bid, stopPrice);
+        console.log(pairPrice);
         rowData.current = {
             fee: 0,
             quantity: size,
