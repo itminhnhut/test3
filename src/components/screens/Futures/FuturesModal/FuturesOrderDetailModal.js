f57b63ac2773c8374044984a7019fd510cb9c8c7
hungtran2606 <hungtn@nami.foundation>
2023-08-28 17:57:48 +0700

    fix: detail order convert symbol

--- src/components/screens/Futures/FuturesModal/FuturesOrderDetailModal.js
+++ src/components/screens/Futures/FuturesModal/FuturesOrderDetailModal.js
@@ -2,7 +2,7 @@ import React, { useState, useCallback, useMemo, useEffect, useRef } from 'react'
 import ModalV2 from 'components/common/V2/ModalV2';
 import { useTranslation } from 'next-i18next';
 import styled from 'styled-components';
-import { CopyText, formatNumber, formatTime, ReasonClose, FeeMetaFutures, countDecimals } from 'redux/actions/utils';
+import { CopyText, formatNumber, formatTime, ReasonClose, FeeMetaFutures, countDecimals, convertSymbol } from 'redux/actions/utils';
 import MiniTickerData from 'components/screens/Futures/MiniTickerData';
 import { DefaultFuturesFee, ApiStatus } from 'redux/actions/const';
 import { createSelector } from 'reselect';
@@ -37,7 +37,7 @@ const FuturesOrderDetailModal = ({ isVisible, onClose, order, decimals, lastPric
     const { t } = useTranslation();
     const allAssets = useSelector((state) => getAllAssets(state));
     const marketWatch = useSelector((state) => state.futures.marketWatch);
-    const pairTicker = marketWatch[order?.symbol];
+    const pairTicker = marketWatch[convertSymbol(order?.symbol)];
     const _lastPrice = pairTicker ? pairTicker?.lastPrice : lastPrice;
     const allPairConfigs = useSelector((state) => state?.futures?.pairConfigs);
 
@@ -45,9 +45,9 @@ const FuturesOrderDetailModal = ({ isVisible, onClose, order, decimals, lastPric
 
     const renderQuoteprice = useCallback(() => {
         return order?.side === VndcFutureOrderType.Side.BUY ? (
-            <MiniTickerData key={order?.displaying_id + 'bid'} initPairPrice={pairTicker} dataKey={'bid'} symbol={order?.symbol} />
+            <MiniTickerData key={order?.displaying_id + 'bid'} initPairPrice={pairTicker} dataKey={'bid'} symbol={convertSymbol(order?.symbol)} />
         ) : (
-            <MiniTickerData key={order?.displaying_id + 'ask'} initPairPrice={pairTicker} dataKey={'ask'} symbol={order?.symbol} />
+            <MiniTickerData key={order?.displaying_id + 'ask'} initPairPrice={pairTicker} dataKey={'ask'} symbol={convertSymbol(order?.symbol)} />
         );
     }, [order]);
 
@@ -55,14 +55,10 @@ const FuturesOrderDetailModal = ({ isVisible, onClose, order, decimals, lastPric
         const size = row?.side === VndcFutureOrderType.Side.SELL ? -row?.quantity : row?.quantity;
         const number = row?.side === VndcFutureOrderType.Side.SELL ? -1 : 1;
         const swap = row?.swap || 0;
-        const liqPrice =
-            (size * row?.open_price +
-                (row?.fee_data?.place_order?.['22'] || 0) +
-                (row?.fee_data?.place_order?.['72'] || 0) +
-                (row?.fee_data?.place_order?.['39'] || 0) +
-                swap -
-                row?.margin) /
-            (row?.quantity * (number - DefaultFuturesFee.Nami));
+        const fee_data = [22, 72, 39].reduce((acc, cur) => {
+            return acc + (row?.fee_data?.place_order?.[cur] || 0);
+        }, 0);
+        const liqPrice = (size * row?.open_price + fee_data + swap - row?.margin) / (row?.quantity * (number - DefaultFuturesFee.Nami));
         return liqPrice > 0 ? formatNumber(liqPrice, 0, decimals.price, false) : '-';
     };
 
@@ -70,8 +66,8 @@ const FuturesOrderDetailModal = ({ isVisible, onClose, order, decimals, lastPric
         const currency = get(order, `fee_metadata[${key}].currency`, get(order, 'margin_currency', null));
         if (!order || !currency) return '-';
         const assetDigit = allAssets?.[currency]?.assetDigit ?? 0;
-        const decimalFunding = currency === 72 || currency === 39 ? 0 : 6;
-        const decimal = key === 'funding_fee.total' ? decimalFunding : currency === 72 || currency === 39 ? assetDigit : assetDigit + 2;
+        const decimalFunding = [72, 39].includes(currency) ? 0 : 6;
+        const decimal = key === 'funding_fee.total' ? decimalFunding : [72, 39].includes(currency) ? assetDigit : assetDigit + 2;
         const assetCode = allAssets?.[currency]?.assetCode ?? '';
         const data = get(order, `fee_metadata[${key}].value`, get(order, key, 0));
         const prefix = negative ? (data < 0 ? '-' : '+') : '';
@@ -414,8 +410,8 @@ const AdjustmentHistory = React.memo(({ id, onClose, pairConfigDetail }) => {
         if (!orderDetail) return '-';
         const currency = orderDetail?.fee_metadata[key]?.currency ?? orderDetail?.margin_currency;
         const assetDigit = allAssets?.[currency]?.assetDigit ?? 0;
-        const decimalFunding = currency === 72 || currency === 39 ? 0 : 6;
-        const decimal = key === 'funding_fee.total' ? decimalFunding : currency === 72 || currency === 39 ? assetDigit : assetDigit + 2;
+        const decimalFunding = [72, 39].includes(currency) ? 0 : 6;
+        const decimal = key === 'funding_fee.total' ? decimalFunding : [72, 39].includes(currency) ? assetDigit : assetDigit + 2;
         const assetCode = allAssets?.[currency]?.assetCode ?? '';
         const data = orderDetail?.fee_metadata[key] ? orderDetail?.fee_metadata[key]['value'] : get(orderDetail, key, 0);
         const prefix = negative ? (data < 0 ? '-' : '+') : '';
@@ -847,7 +843,7 @@ const FeeMeta = React.memo(({ orderDetail, mode = 'open_fee', allAssets, t }) =>
                 <div className="mt-2 rounded-md w-full grid grid-cols-2 gap-2 border dark:border-divider-dark p-3">
                     {fee_metadata.map((rs, idx) => (
                         <div className={`text-base font-semibold text-darkBlue dark:text-gray-4 ${idx % 2 === 0 ? 'text-left' : 'text-right'}`} key={idx}>
-                            {formatNumber(rs.value, rs.asset === 72 || rs.asset === 39 ? allAssets[rs.asset].assetDigit : allAssets[rs.asset].assetDigit + 2)}{' '}
+                            {formatNumber(rs.value, [72, 39].includes(rs?.asset) ? allAssets[rs.asset].assetDigit : allAssets[rs.asset].assetDigit + 2)}{' '}
                             {allAssets[rs.asset].assetCode}
                         </div>
                     ))}
