598c74c66fb31fe22eac6f9f65f821e3da6e8503
giangnt <gr3en4pple23@gmail.com>
2023-08-31 11:28:22 +0700

    feat: updating mobile view for insurance rules

--- src/components/screens/Insurance/InsuranceRules.js
+++ src/components/screens/Insurance/InsuranceRules.js
@@ -6,21 +6,97 @@ import { API_CONFIG_INSURANCE_RULE } from 'redux/actions/apis';
 import colors from 'styles/colors';
 import axios from 'axios';
 import FetchApi from 'utils/fetch-api';
-import { filterSearch, getSymbolObject } from 'redux/actions/utils';
+import { filterSearch, formatNumber, getSymbolObject } from 'redux/actions/utils';
 import AssetLogo from 'components/wallet/AssetLogo';
 import SearchBoxV2 from 'components/common/SearchBoxV2';
 import classNames from 'classnames';
 import Tooltip from 'components/common/Tooltip';
 import { useWindowSize } from 'utils/customHooks';
 import { useTranslation } from 'next-i18next';
+import Button from 'components/common/V2/ButtonV2/Button';
+import Skeletor from 'components/common/Skeletor';
+import TableMobileView from './TableMobileView';
 
 const ROWS_PER_PAGE = 10;
+export const MOBILE_ROWS_PER_PAGE = 5;
 
-const MIN_MARGIN = 5;
-const MIN_MAX_RATIO = {
+export const MIN_MARGIN = 5;
+export const MIN_MAX_RATIO = {
     min: 2,
     max: 10
 };
+const getColumns = (t) => [
+    {
+        key: 'symbol',
+        dataIndex: 'symbol',
+        title: t('common:asset'),
+        align: 'left',
+        fixed: 'left',
+        width: 175,
+        render: (symbol, item) => {
+            const symbolObject = getSymbolObject(symbol);
+            return (
+                <div className="flex space-x-2 items-center mb-2">
+                    <AssetLogo assetCode={symbolObject?.baseAsset} size={24} useNextImg />
+                    <span className="font-semibold">{symbol}</span>
+                </div>
+            );
+        },
+        className: 'custom-style'
+    },
+    {
+        key: 'avg_changing',
+        dataIndex: 'avg_changing',
+        title: (
+            <span data-for="avg_changing_detail" data-tip="" className="inline-block nami-underline-dotted">
+                {t('futures:insurance.difference')}
+            </span>
+        ),
+        align: 'left',
+        width: 262,
+        render: (data, item) => {
+            return <div className="">{formatNumber(data * 100, 2)}%</div>;
+        }
+    },
+    {
+        key: 'max_leverage',
+        dataIndex: 'max_leverage',
+        title: <span className="inline-block ">{t('futures:insurance.max_leverage')}</span>,
+        align: 'left',
+        width: 262,
+        render: (data, item) => {
+            return <div className="">{data}x</div>;
+        }
+    },
+    {
+        key: 'min_margin',
+        dataIndex: 'min_margin',
+        title: <span className="inline-block ">{t('futures:insurance.min_margin')}</span>,
+        align: 'left',
+        width: 262,
+        render: (_) => {
+            return <div className="">{MIN_MARGIN} USDT</div>;
+        }
+    },
+    {
+        key: 'minmax_ratio',
+        dataIndex: 'minmax_ratio',
+        title: (
+            <span className="inline-block nami-underline-dotted" data-for="minmax_ratio" data-tip="">
+                {t('futures:insurance.min_max_margin_ratio')}
+            </span>
+        ),
+        align: 'left',
+        width: 262,
+        render: (_) => {
+            return (
+                <div className="">
+                    {MIN_MAX_RATIO.min}%/{MIN_MAX_RATIO.max}%
+                </div>
+            );
+        }
+    }
+];
 
 const InsuranceRules = () => {
     const {
@@ -31,8 +107,8 @@ const InsuranceRules = () => {
     const [rulesData, set] = useState({
         data: [],
         loading: true,
-        maxPage: 1,
-        page: 1
+        page: 1,
+        mobileViewPage: 1
     });
     const [search, setSearch] = useState('');
 
@@ -57,8 +133,7 @@ const InsuranceRules = () => {
                 if (data) {
                     setRulesData({
                         data: data?.data || [],
-                        loading: false,
-                        maxPage: Math.ceil((data?.data?.length ?? 0) / ROWS_PER_PAGE)
+                        loading: false
                     });
                 }
             } catch (error) {
@@ -71,88 +146,18 @@ const InsuranceRules = () => {
 
     const filterRulesData = useMemo(() => {
         const copiedData = [...rulesData.data];
-
+        setRulesData({ page: 1, mobileViewPage: 1 });
         return filterSearch(copiedData, ['symbol'], search);
     }, [rulesData.data, search]);
 
-    const columns = useMemo(
-        () => [
-            {
-                key: 'symbol',
-                dataIndex: 'symbol',
-                title: t('common:asset'),
-                align: 'left',
-                fixed: 'left',
-                width: 175,
-                render: (symbol, item) => {
-                    const symbolObject = getSymbolObject(symbol);
-                    return (
-                        <div className="flex space-x-2 items-center mb-2">
-                            <AssetLogo assetCode={symbolObject?.baseAsset} size={24} useNextImg />
-                            <span className="font-semibold">{symbol}</span>
-                        </div>
-                    );
-                },
-                className: 'custom-style'
-            },
-            {
-                key: 'avg_changing',
-                dataIndex: 'avg_changing',
-                title: (
-                    <span data-for="avg_changing_detail" data-tip="" className="inline-block nami-underline-dotted">
-                        {t('futures:insurance.difference')}
-                    </span>
-                ),
-                align: 'left',
-                width: 262,
-                render: (data, item) => {
-                    return <div className="">{data * 100}%</div>;
-                }
-            },
-            {
-                key: 'max_leverage',
-                dataIndex: 'max_leverage',
-                title: <span className="inline-block ">{t('futures:insurance.max_leverage')}</span>,
-                align: 'left',
-                width: 262,
-                render: (data, item) => {
-                    return <div className="">{data}x</div>;
-                }
-            },
-            {
-                key: 'min_margin',
-                dataIndex: 'min_margin',
-                title: <span className="inline-block ">{t('futures:insurance.min_margin')}</span>,
-                align: 'left',
-                width: 262,
-                render: (_) => {
-                    return <div className="">{MIN_MARGIN} USDT</div>;
-                }
-            },
-            {
-                key: 'minmax_ratio',
-                dataIndex: 'minmax_ratio',
-                title: (
-                    <span className="inline-block nami-underline-dotted" data-for="minmax_ratio" data-tip="">
-                        {t('futures:insurance.min_max_margin_ratio')}
-                    </span>
-                ),
-                align: 'left',
-                width: 262,
-                render: (_) => {
-                    return (
-                        <div className="">
-                            {MIN_MAX_RATIO.min}%/{MIN_MAX_RATIO.max}%
-                        </div>
-                    );
-                }
-            }
-        ],
-        [t]
-    );
+    const maxPage = useMemo(() => {
+        const isMobile = width <= 820;
+
+        return Math.ceil((filterRulesData?.length ?? 0) / (isMobile ? MOBILE_ROWS_PER_PAGE : ROWS_PER_PAGE));
+    }, [filterRulesData?.length, width]);
 
     return (
-        <div className={classNames('mt-10 sm:mt-20 mx-4 pb-[120px]')}>
+        <div className={classNames('mt-10 mb-20 px-4 mb:mb-[120px] mb:mt-20 ')}>
             <Tooltip className="p-6 max-w-[247px] text-center" id="avg_changing_detail" place="top" effect="solid" isV3>
                 {t('futures:insurance.difference_description')}
             </Tooltip>
@@ -160,26 +165,40 @@ const InsuranceRules = () => {
                 {t('futures:insurance.min_max_margin_ratio_description')}
             </Tooltip>
             <div className="max-w-screen-v3 2xl:max-w-screen-xxl m-auto">
-                <div className="flex items-center justify-between mb-8">
-                    <div className="text-4xl font-semibold flex-1 ">{t('futures:insurance.insurance_rules_title')}</div>
+                <div className="flex items-center justify-between gap-6 flex-wrap mb-10 mb:mb-8">
+                    <div className="text-xl mb:text-4xl font-semibold ">{t('futures:insurance.insurance_rules_title')}</div>
 
                     <SearchBoxV2
-                        wrapperClassname="max-w-[368px]"
+                        wrapperClassname="w-full h-11 mb:h-[50px] mb:max-w-[368px]"
                         value={search}
                         onChange={(value) => {
                             setSearch(value);
                         }}
                     />
                 </div>
-                <Wrapper ref={containerRef} searchWidth={noDataContainerWidth} isDarkMode={currentTheme === THEME_MODE.DARK}>
+                <div className="mb:hidden">
+                    <div className="space-y-8 mb-6">
+                        <TableMobileView
+                            filterRulesData={filterRulesData}
+                            page={rulesData.mobileViewPage}
+                            loading={rulesData.loading}
+                            isSearch={Boolean(search)}
+                        />
+                    </div>
+                    {rulesData.mobileViewPage < maxPage && (
+                        <Button variants="text" className="!h-9" onClick={() => setRulesData({ mobileViewPage: rulesData.mobileViewPage + 1 })}>
+                            {t('common:ext_gate.see_more')}
+                        </Button>
+                    )}
+                </div>
+                <Wrapper className="hidden mb:block" ref={containerRef} searchWidth={noDataContainerWidth} isDarkMode={currentTheme === THEME_MODE.DARK}>
                     <TableV2
                         useRowHover
                         data={filterRulesData}
-                        columns={columns}
+                        columns={getColumns(t)}
                         rowKey={(item) => `${item?.key}`}
                         loading={rulesData.loading}
                         limit={ROWS_PER_PAGE}
-                        skip={1}
                         page={rulesData.page}
                         isSearch={search}
                         className="border border-divider dark:border-divider-dark rounded-xl pt-1"
@@ -189,10 +208,13 @@ const InsuranceRules = () => {
                         total={filterRulesData?.length ?? 0}
                         pagingPrevNext={{
                             page: rulesData.page,
-                            hasNext: rulesData.page < rulesData.maxPage,
-                            onChangeNextPrev: (e) => setRulesData({ page: rulesData.page + e }),
+                            hasNext: rulesData.page < maxPage,
+                            onChangeNextPrev: (e) => {
+                                setRulesData({ page: rulesData.page + e });
+                            },
                             language
                         }}
+                        initPage={1}
                     />
                 </Wrapper>
             </div>
---
+++ src/components/screens/Insurance/TableMobileView.js
@@ -0,0 +1,80 @@
+import Skeletor from 'components/common/Skeletor';
+import React from 'react';
+import { MIN_MARGIN, MIN_MAX_RATIO, MOBILE_ROWS_PER_PAGE } from './InsuranceRules';
+import { formatNumber, getSymbolObject } from 'redux/actions/utils';
+import AssetLogo from 'components/wallet/AssetLogo';
+import { useTranslation } from 'next-i18next';
+import NoData from 'components/common/V2/TableV2/NoData';
+
+const TableMobileView = ({ filterRulesData, loading, isSearch, page }) => {
+    const { t } = useTranslation();
+    return (
+        <div className="space-y-8 mb-6">
+            {loading ? (
+                // LOADING
+                [...Array(2).keys()].map((key) => (
+                    <div key={key} className="space-y-4 last:pb-0 pb-4 last:border-0 border-b border-divider dark:border-divider-dark">
+                        <div className="flex items-center font-semibold space-x-2">
+                            <Skeletor circle width={24} height={24} />
+                            <Skeletor width={70} />
+                        </div>
+                        <div className="flex items-center text-sm justify-between">
+                            <Skeletor width={100} />
+                            <Skeletor width={100} />
+                        </div>
+                        <div className="flex items-center text-sm justify-between">
+                            <Skeletor width={100} />
+                            <Skeletor width={100} />
+                        </div>
+                        <div className="flex items-center text-sm justify-between">
+                            <Skeletor width={100} />
+                            <Skeletor width={100} />
+                        </div>
+                        <div className="flex items-center text-sm justify-between">
+                            <Skeletor width={100} />
+                            <Skeletor width={100} />
+                        </div>
+                    </div>
+                ))
+            ) : // NO DATA
+            !filterRulesData?.length && isSearch ? (
+                <NoData loading={loading} isSearch className="!text-base" />
+            ) : (
+                // MAPPING DATA
+                [...filterRulesData].slice(0, MOBILE_ROWS_PER_PAGE * page)?.map((rule, i) => {
+                    const symbolObject = getSymbolObject(rule?.symbol);
+                    return (
+                        <div key={i} className="space-y-4 last:pb-0 pb-4 last:border-0 border-b border-divider dark:border-divider-dark">
+                            <div className="flex items-center font-semibold space-x-2">
+                                <AssetLogo assetCode={symbolObject?.baseAsset} size={24} useNextImg />
+                                <div className="">{rule?.symbol}</div>
+                            </div>
+                            <div className="flex items-center text-sm justify-between">
+                                <div data-for="avg_changing_detail" data-tip="" className="text-txtSecondary dark:text-txtSecondary-dark nami-underline-dotted">
+                                    {t('futures:insurance.difference')}
+                                </div>
+                                <div>{formatNumber(rule?.avg_changing * 100, 2)}%</div>
+                            </div>
+                            <div className="flex items-center text-sm justify-between">
+                                <div className="text-txtSecondary dark:text-txtSecondary-dark">{t('futures:insurance.max_leverage')}</div>
+                                <div>{rule?.max_leverage}x</div>
+                            </div>
+                            <div className="flex items-center text-sm justify-between">
+                                <div className="text-txtSecondary dark:text-txtSecondary-dark">{t('futures:insurance.min_margin')}</div>
+                                <div>{MIN_MARGIN} USDT</div>
+                            </div>
+                            <div className="flex items-center text-sm justify-between">
+                                <div className="text-txtSecondary dark:text-txtSecondary-dark">{t('futures:insurance.min_max_margin_ratio')}</div>
+                                <div className="">
+                                    {MIN_MAX_RATIO.min}%/{MIN_MAX_RATIO.max}%
+                                </div>
+                            </div>
+                        </div>
+                    );
+                })
+            )}
+        </div>
+    );
+};
+
+export default TableMobileView;
