e22b565eb39120dad528c0f9aa66fd7b8a8195c4
giangnt <gr3en4pple23@gmail.com>
2023-09-05 11:22:58 +0700

    style: layout profile partner

--- src/components/screens/WithdrawDeposit/constants.js
+++ src/components/screens/WithdrawDeposit/constants.js
@@ -274,13 +274,14 @@ export const fiatFilter = [
         localized: null
     },
     {
-        key: 72,
-        localized: ALLOWED_ASSET['72']
-    },
-    {
         key: 39,
         localized: ALLOWED_ASSET['39']
     },
+    {
+        key: 72,
+        localized: ALLOWED_ASSET['72']
+    },
+
     {
         key: 22,
         localized: ALLOWED_ASSET['22']
