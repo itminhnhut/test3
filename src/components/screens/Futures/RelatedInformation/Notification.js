b7690dc9abce6f51926a2c62508873726eef1439
nhatlong1303 <nguyennhatlong1303@gmail.com>
2023-10-04 14:38:06 +0700

    Futures - NAO - update:time

--- src/components/screens/Futures/RelatedInformation/Notification.js
+++ src/components/screens/Futures/RelatedInformation/Notification.js
@@ -90,11 +90,11 @@ const index = memo(({ platform, symbol }) => {
             : dataSource.filter((item) => {
                   const time = expire[item.id];
                   const diffTimeStamp = now - time;
-                  // const diffHours = diffTimeStamp / (1000 * 60 * 60);
-                  // isShowAlert = diffHours >= 8;
-                  const diffMinutes = diffTimeStamp / (1000 * 60);
-                  if (time) console.log(`${symbol}_${item.id}_${diffMinutes}`);
-                  return diffMinutes >= 5 || !time;
+                  const diffHours = diffTimeStamp / (1000 * 60 * 60);
+                  isShowAlert = diffHours >= 8 || !time;
+                  //   const diffMinutes = diffTimeStamp / (1000 * 60);
+                  //   if (time) console.log(`${symbol}_${item.id}_${diffMinutes}`);
+                  //   return diffMinutes >= 5 || !time;
               });
         setDataFilter(_dataFilter);
     }, [dataSource]);
