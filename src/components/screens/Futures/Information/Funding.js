f70e03e3b932b72c5f635c7d639b0d0b9fdd6db0
nhatlong1303 <nguyennhatlong1303@gmail.com>
2023-10-09 15:50:03 +0700

    Futures - Funding rate - fix:crash

--- src/components/screens/Futures/Information/Funding.js
+++ src/components/screens/Futures/Information/Funding.js
@@ -3,18 +3,16 @@ import { useSelector } from 'react-redux';
 import { convertSymbol, formatFundingRateV2 } from 'redux/actions/utils';
 import { getMarketWatch } from 'redux/selectors';
 import Countdown from 'react-countdown-now';
-import { PublicSocketEvent } from 'redux/actions/const';
-import Emitter from 'redux/actions/emitter';
-import FuturesMarketWatch from 'models/FuturesMarketWatch';
 import Tooltip from 'components/common/Tooltip';
 import { useTranslation } from 'next-i18next';
 
 const Funding = ({ pairPrice, symbol, className = '', buyClassName = '', sellClassName = '' }) => {
-    const ticker = pairPrice ? pairPrice : useSelector((state) => getMarketWatch(state, convertSymbol(symbol)));
+    const marketWatch = useSelector((state) => getMarketWatch(state, convertSymbol(symbol)));
+    const ticker = pairPrice ?? marketWatch;
     const publicSocket = useSelector((state) => state.socket.publicSocket);
 
     useEffect(() => {
-        if (!symbol || ticker) return;
+        if (!symbol || ticker || !publicSocket) return;
         const _symbol = convertSymbol(symbol);
         publicSocket.emit('subscribe:futures:ticker', _symbol);
     }, [publicSocket, symbol]);
@@ -30,22 +28,9 @@ const Funding = ({ pairPrice, symbol, className = '', buyClassName = '', sellCla
 
 const FundingCountdown = ({ pairPrice, symbol, tooltip, className = '' }) => {
     const { t } = useTranslation();
+    const marketWatch = useSelector((state) => getMarketWatch(state, convertSymbol(symbol)));
     const timesync = useSelector((state) => state.utils.timesync);
-    const [ticker, setTicker] = useState(pairPrice);
-
-    useEffect(() => {
-        setTicker(pairPrice);
-    }, [pairPrice]);
-
-    useEffect(() => {
-        if (pairPrice) return;
-        Emitter.on(PublicSocketEvent.FUTURES_TICKER_UPDATE + symbol, async (data) => {
-            if (data?.s && data?.p > 0) {
-                const _pairPrice = FuturesMarketWatch.create(data);
-                setTicker(_pairPrice);
-            }
-        });
-    }, [symbol]);
+    const ticker = pairPrice ?? marketWatch;
 
     if (!ticker) return '-';
     return (
